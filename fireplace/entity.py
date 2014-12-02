5625672012bdc4742e8814f85d5fefcf60de91bd
Jerome Leclanche <jerome@leclan.ch>
2014-12-02 06:40:08 +0100

    Implement Entity.getIntProperty() and getBoolProperty()
    This stuff will replace getProperty() soon enough.

--- fireplace/entity.py
+++ fireplace/entity.py
@@ -7,3 +7,17 @@ class Entity(object):
 
 	def unsetTag(self, tag):
 		del self.tags[tag]
+
+	def getIntProperty(self, tag):
+		ret = self.tags.get(tag, 0)
+		for slot in self.slots:
+			ret += slot.getIntProperty(tag)
+		return ret
+
+	def getBoolProperty(self, tag):
+		if self.tags.get(tag, False):
+			return True
+		for slot in self.slots:
+			if slot.getBoolProperty(tag):
+				return True
+		return
