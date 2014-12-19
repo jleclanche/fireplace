a287c1e7a6e96a2a2143e9270a5f9b2ec295022e
Jerome Leclanche <jerome@leclan.ch>
2014-12-19 23:07:56 +0000

    Implement the old Dagger Mastery

--- fireplace/cards/removed/all.py
+++ fireplace/cards/removed/all.py
@@ -5,6 +5,18 @@ Cards removed from the game
 from ..utils import *
 
 
+# Dagger Mastery
+class CS2_083:
+	def action(self):
+		if self.hero.weapon:
+			self.hero.weapon.buff("CS2_083e")
+		else:
+			self.hero.summon("CS2_082")
+
+class CS2_083e:
+	Atk = 1
+
+
 # Adrenaline Rush
 class NEW1_006:
 	action = drawCard
