fa7c38b69d849103c5f80b3a56608d4df4c4b0c5
Jerome Leclanche <jerome@leclan.ch>
2014-12-23 15:08:19 +0000

    Implement Echo of Medivh

--- fireplace/cards/gvg/mage.py
+++ fireplace/cards/gvg/mage.py
@@ -8,3 +8,10 @@ from ..utils import *
 class GVG_001:
 	def action(self):
 		self.hit(random.choice(self.controller.opponent.field), 4)
+
+
+# Echo of Medivh
+class GVG_005:
+	def action(self):
+		for minion in self.controller.field:
+			self.controller.give(minion.id)
