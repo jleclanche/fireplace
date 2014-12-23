42f74f304d0ac404f17d6489033b6140816cb194
Jerome Leclanche <jerome@leclan.ch>
2014-12-23 19:42:39 +0000

    Implement Stonesplinter Trogg, Burly Rockjaw Trogg, Ship's Cannon

--- fireplace/cards/gvg/neutral_common.py
+++ fireplace/cards/gvg/neutral_common.py
@@ -4,6 +4,34 @@ from ..utils import *
 ##
 # Minions
 
+# Stonesplinter Trogg
+class GVG_067:
+	def CARD_PLAYED(self, player, card):
+		if player is not self.controller and card.type == CardType.SPELL:
+			self.buff("GVG_067a")
+
+class GVG_067a:
+	Atk = 1
+
+
+# Burly Rockjaw Trogg
+class GVG_068:
+	def CARD_PLAYED(self, player, card):
+		if player is not self.controller and card.type == CardType.SPELL:
+			self.buff("GVG_068a")
+
+class GVG_068a:
+	Atk = 2
+
+
+# Ship's Cannon
+class GVG_075:
+	def OWN_MINION_SUMMONED(self, minion):
+		if minion.race == Race.PIRATE:
+			targets = self.controller.getTargets(TARGET_ENEMY_CHARACTERS)
+			self.hit(random.choice(targets), 2)
+
+
 # Explosive Sheep
 class GVG_076:
 	def deathrattle(self):
