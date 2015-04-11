2a3bb1b870dde2a3a8560c0f1baf6c2c3da22e25
Jerome Leclanche <jerome@leclan.ch>
2015-04-12 01:43:36 +0200

    Implement OptionType / PowType / PowSubType enums

--- fireplace/enums.py
+++ fireplace/enums.py
@@ -244,6 +244,35 @@ class PlayReq(IntEnum):
 	REQ_SOURCE_IS_ENRAGED = -5
 
 
+class OptionType(IntEnum):
+	PASS = 1
+	END_TURN = 2
+	POWER = 3
+
+
+class PowType(IntEnum):
+	FULL_ENTITY = 1
+	SHOW_ENTITY = 2
+	HIDE_ENTITY = 3
+	TAG_CHANGE = 4
+	ACTION_START = 5
+	ACTION_END = 6
+	CREATE_GAME = 7
+	META_DATA = 8
+
+
+class PowSubType(IntEnum):
+	ATTACK = 1
+	CONTINUOUS = 2
+	POWER = 3
+	SCRIPT = 4
+	TRIGGER = 5
+	DEATHS = 6
+	PLAY = 7
+	FATIGUE = 8
+	ACTION = 99
+
+
 class AuraType(IntEnum):
 	PLAY_AURA = 1
 	HAND_AURA = 2
