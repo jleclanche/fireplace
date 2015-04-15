22d72a2daf1cd7ba46ded5f75a2322357762a86c
Jerome Leclanche <jerome@leclan.ch>
2015-04-15 23:14:07 +0200

    Implement Anodized Robo Cub, Grove Tender

--- fireplace/cards/gvg/druid.py
+++ fireplace/cards/gvg/druid.py
@@ -4,6 +4,29 @@ from ..utils import *
 ##
 # Minions
 
+# Attack Mode (Anodized Robo Cub)
+class GVG_030a:
+	action = buffSelf("GVG_030ae")
+
+# Tank Mode (Anodized Robo Cub)
+class GVG_030b:
+	action = buffSelf("GVG_030be")
+
+
+# Gift of Mana (Grove Tender)
+class GVG_032a:
+	def action(self):
+		for player in self.game.players:
+			player.maxMana += 1
+			player.usedMana -= 1
+
+# Gift of Cards (Grove Tender)
+class GVG_032b:
+	def action(self):
+		for player in self.game.players:
+			player.draw()
+
+
 # Druid of the Fang
 class GVG_080:
 	def action(self):
