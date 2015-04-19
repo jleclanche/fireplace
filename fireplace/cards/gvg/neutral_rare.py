c082853e89ae2f6e9cb20681a4f06f8231477528
Jerome Leclanche <jerome@leclan.ch>
2015-04-19 01:51:43 +0000

    Implement Goblin Sapper
    Closes #23

--- fireplace/cards/gvg/neutral_rare.py
+++ fireplace/cards/gvg/neutral_rare.py
@@ -4,6 +4,14 @@ from ..utils import *
 ##
 # Minions
 
+# Goblin Sapper
+class GVG_095:
+	def atk(self, i):
+		if len(self.controller.opponent.hand) >= 6:
+			return i + 4
+		return i
+
+
 # Bomb Lobber
 class GVG_099:
 	def action(self):
