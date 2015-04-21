09dd2e16ef29b6c79ee344a55bea5bd0e59c7a59
Jerome Leclanche <jerome@leclan.ch>
2015-04-21 12:53:44 +0000

    Implement Powermace, Crackle, Vitality Totem and Siltfin Spiritwalker

--- fireplace/cards/gvg/shaman.py
+++ fireplace/cards/gvg/shaman.py
@@ -2,6 +2,21 @@ from ..utils import *
 
 
 ##
+# Minions
+
+# Vitality Totem
+class GVG_039:
+	OWN_TURN_END = [Heal(FRIENDLY_HERO, 4)]
+
+
+# Siltfin Spiritwalker
+class GVG_040:
+	def OWN_MINION_DESTROY(self, minion):
+		if minion.race == Race.MURLOC:
+			return [Draw(CONTROLLER, 1)]
+
+
+##
 # Spells
 
 # Ancestor's Call
@@ -10,3 +25,17 @@ class GVG_029:
 		ForcePlay(CONTROLLER, RANDOM(CONTROLLER_HAND + MINION)),
 		ForcePlay(OPPONENT, RANDOM(OPPONENT_HAND + MINION)),
 	]
+
+
+# Crackle
+class GVG_038:
+	def action(self, target):
+		return [Hit(TARGET, random.randint(3, 6))]
+
+
+##
+# Weapons
+
+# Powermace
+class GVG_036:
+	action = [Buff(RANDOM(FRIENDLY_MINIONS + MECH), "GVG_036e")]
