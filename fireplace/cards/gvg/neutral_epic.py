572a84ae4fe7ce464fe66b6462a80b09b20f8f1c
Jerome Leclanche <jerome@leclan.ch>
2015-04-21 18:15:52 +0000

    Implement Piloted Sky Golem, Junkbot and Enhance-o Mechano

--- fireplace/cards/gvg/neutral_epic.py
+++ fireplace/cards/gvg/neutral_epic.py
@@ -9,3 +9,24 @@ class GVG_104:
 	def OWN_CARD_PLAYED(self, card):
 		if card.type == CardType.MINION and card.atk == 1:
 			return [Buff(card, "GVG_104a")]
+
+
+# Piloted Sky Golem
+class GVG_105:
+	def deathrattle(self):
+		return [Summon(CONTROLLER, randomCollectible(type=CardType.MINION, cost=4))]
+
+
+# Junkbot
+class GVG_106:
+	def OWN_MINION_DESTROY(self, minion):
+		if minion.race == Race.MECHANICAL:
+			return [Buff(SELF, "GVG_106e")]
+
+
+# Enhance-o Mechano
+class GVG_107:
+	def action(self):
+		for target in self.controller.field:
+			tag = random.choice((GameTag.WINDFURY, GameTag.TAUNT, GameTag.DIVINE_SHIELD))
+			yield SetTag(target, {tag: True})
