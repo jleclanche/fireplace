5da820b85f9e55a54639856bdd698c35b866833c
Jerome Leclanche <jerome@leclan.ch>
2015-04-21 16:40:26 +0000

    Fix Hobgoblin to trigger only on cards played

--- fireplace/cards/gvg/neutral_epic.py
+++ fireplace/cards/gvg/neutral_epic.py
@@ -6,6 +6,6 @@ from ..utils import *
 
 # Hobgoblin
 class GVG_104:
-	def OWN_MINION_SUMMON(self, minion):
-		if minion.atk == 1:
-			return [Buff(minion, "GVG_104a")]
+	def OWN_CARD_PLAYED(self, card):
+		if card.type == CardType.MINION and card.atk == 1:
+			return [Buff(card, "GVG_104a")]
