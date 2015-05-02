af17b2d324f91ad69b489fe833cac1116e992f80
pkappler <pkappler@gmail.com>
2015-05-02 13:15:14 -0500

    fix import errors, player setup
    Test still fails, but at least it runs now.

--- tests/full_game.py
+++ tests/full_game.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 import sys; sys.path.append("..")
-import fireplace
+from fireplace import heroes
+from fireplace import player
+from fireplace import game
 import logging
 from fireplace.utils import randomDraft
 
@@ -8,16 +10,19 @@ logging.getLogger().setLevel(logging.DEBUG)
 
 
 def main():
-	deck1 = randomDraft(hero=fireplace.heroes.MAGE)
-	deck2 = randomDraft(hero=fireplace.heroes.WARRIOR)
-	player1 = fireplace.Player(name="Player1", deck=deck1)
-	player2 = fireplace.Player(name="Player2", deck=deck2)
 
-	game = fireplace.Game(players=(player1, player2))
-	game.start()
+	deck1 = randomDraft(hero=heroes.MAGE)
+	deck2 = randomDraft(hero=heroes.WARRIOR)
+	player1 = player.Player(name="Player1")
+	player1.prepareDeck(deck1, heroes.MAGE)
+	player2 = player.Player(name="Player2")
+	player2.prepareDeck(deck2, heroes.WARRIOR)
+
+	gm = game.Game(players=(player1, player2))
+	gm.start()
 
 	while True:
-		heropower = game.currentPlayer.hero.power
+		heropower = gm.currentPlayer.hero.power
 		# always play the hero power, just for kicks
 		if heropower.isPlayable():
 			if heropower.hasTarget():
@@ -25,7 +30,7 @@ def main():
 			else:
 				heropower.play()
 		# iterate over our hand and play whatever is playable
-		for card in game.currentPlayer.hand:
+		for card in gm.currentPlayer.hand:
 			if card.isPlayable():
 				if card.hasTarget():
 					card.play(target=card.targets[0])
@@ -34,14 +39,14 @@ def main():
 			else:
 				print("Not playing", card)
 		# attack with whatever minions can attack
-		for minion in game.currentPlayer.field:
+		for minion in gm.currentPlayer.field:
 			if minion.canAttack():
-				minion.attack(game.currentPlayer.opponent.hero)
+				minion.attack(gm.currentPlayer.opponent.hero)
 		# attack with the hero
-		if game.currentPlayer.hero.canAttack():
+		if gm.currentPlayer.hero.canAttack():
 			print("Attacking with the hero")
-			game.currentPlayer.hero.attack(game.currentPlayer.opponent.hero)
-		game.endTurn()
+			gm.currentPlayer.hero.attack(gm.currentPlayer.opponent.hero)
+		gm.endTurn()
 
 
 if __name__ == "__main__":
