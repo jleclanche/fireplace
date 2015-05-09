44ef488bbe25576ba25ca5855b726fa16fffa8bc
Jerome Leclanche <jerome@leclan.ch>
2015-05-09 14:58:09 +0200

    Implement Flamewaker and correct Dragon's Breath id

--- fireplace/cards/blackrock/collectible.py
+++ fireplace/cards/blackrock/collectible.py
@@ -2,10 +2,20 @@ from ..utils import *
 
 
 ##
+# Minions
+
+# Flamewaker
+class BRM_002:
+	events = [
+		OWN_SPELL_PLAY.after(Hit(RANDOM_ENEMY_MINION, 1) * 2)
+	]
+
+
+##
 # Spells
 
 # Dragon's Breath
-class DragonsBreath:
+class BRM_003:
 	action = [Hit(TARGET, 4)]
 
 	def cost(self, value):
