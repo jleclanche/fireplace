d83d2694819b7b948ee8fdee9eaf5a1d0cfd25cd
Jerome Leclanche <jerome@leclan.ch>
2015-06-19 08:18:05 +0200

    Implement Flameheart

--- fireplace/cards/blackrock/__init__.py
+++ fireplace/cards/blackrock/__init__.py
@@ -1,1 +1,2 @@
+from .adventure import *
 from .collectible import *
---
+++ fireplace/cards/blackrock/adventure.py
@@ -0,0 +1,9 @@
+from ..utils import *
+
+
+##
+# Spells
+
+# Flameheart
+class BRMA_01:
+	action = [Draw(CONTROLLER) * 2, GainArmor(CONTROLLER, 4)]
