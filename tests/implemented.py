d5d92aa7686abe3f18a940880a3a173b9b4d95a0
laiqu <laiquendir@gmail.com>
2015-07-03 16:17:12 +0200

    implemented.py: Improve implementation checking

--- tests/implemented.py
+++ tests/implemented.py
@@ -1,36 +1,68 @@
 #!/usr/bin/env python
 import os
+import re
+import string
 import sys; sys.path.append(".."); sys.path.append("../data/extras")
+from data.extras import chooseone
 from fireplace import cards
-from fireplace.cards import debug, game, classic, naxxramas, gvg
+from fireplace.cards import debug, game, classic, naxxramas, gvg, blackrock
 from fireplace.enums import CardType
 
 import buffs
 
 
 GREEN = "\033[92m"
+YELLOW = "\033[33m"
 RED = "\033[91m"
 ENDC = "\033[0m"
+PREFIXES = {
+	GREEN: "Implemented: ",
+	YELLOW: "Potentially implemented: ",
+	RED: "Not implemented: ",
+}
+
+SOLVED_KEYWORDS = [
+    "Windfury", "Charge", "Divine Shield", "Taunt", "Stealth",
+    "Spell Damage \+[0-9]+",
+    "Can't be targeted by spells or Hero Powers",
+    "Overload: \([0-9]+\)",
+    "50% chance to attack the wrong enemy",
+    "Can't Attack",
+]
+
+def cleanup_description(description):
+	ret = description
+	ret = re.sub("(" + "|".join(SOLVED_KEYWORDS) + ")", "", ret)
+	ret = re.sub("<[^>]*>", "", ret)
+	exclude_chars = string.punctuation + string.whitespace
+	ret = ''.join([ ch for ch in ret if ch not in exclude_chars])
+	return ret
+
+
+def potentially_implemented(card):
+	return card.auras or "Enrage" in card.description or hasattr(chooseone, card.id)
 
 
 def main():
 	for id in sorted(cards.db):
 		card = cards.db[id]
-		for set in (debug, game, classic, naxxramas, gvg):
-			if hasattr(set, id):
+		if not cleanup_description(card.description):
+			# Minions without card text or with basic abilities are implemented
+			color = GREEN
+		elif card.type == CardType.ENCHANTMENT:
+			if id in buffs.__dict__:
 				color = GREEN
-				break
-			elif not card.description:
-				# Minions without card text are implemented
-				color = GREEN
-				break
-			elif card.type == CardType.ENCHANTMENT:
-				if id in buffs.__dict__:
+		else:
+			for set in (debug, game, classic, naxxramas, gvg, blackrock):
+				if hasattr(set, id):
 					color = GREEN
 					break
-		else:
-			color = RED
-		print(color + ("not " if color != GREEN else "") +  "implemented: " + card.name + ENDC + " (%s)" % (id))
+			else:
+				if potentially_implemented(card):
+					color = YELLOW
+				else:
+					color = RED
+		print(color + PREFIXES[color] + card.name + ENDC + " (%s)" % (id))
 
 
 if __name__ == "__main__":
