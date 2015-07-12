c38bf4b223dfbb604900417be972a9e6f4119645
Jerome Leclanche <jerome@leclan.ch>
2015-07-12 17:06:58 +0200

    Implement enums.CardSet

--- fireplace/enums.py
+++ fireplace/enums.py
@@ -152,6 +152,29 @@ class CardClass(IntEnum):
 	COUNT = 12
 
 
+class CardSet(IntEnum):
+	INVALID = 0
+	TEST_TEMPORARY = 1
+	CORE = 2
+	EXPERT1 = 3
+	REWARD = 4
+	MISSIONS = 5
+	DEMO = 6
+	NONE = 7
+	CHEAT = 8
+	BLANK = 9
+	DEBUG_SP = 10
+	PROMO = 11
+	FP1 = 12
+	PE1 = 13
+	BRM = 14
+	TEMP1 = 15
+	CREDITS = 16
+	HERO_SKINS = 17
+	TB = 18
+	SLUSH = 19
+
+
 class CardType(IntEnum):
 	INVALID = 0
 	GAME = 1
