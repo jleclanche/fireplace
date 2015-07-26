b881d93fc33cd3839a544d01fb5d2dd62726322b
Jerome Leclanche <jerome@leclan.ch>
2015-07-26 20:36:06 +0200

    Implement more Blackrock Brawl cards

--- fireplace/cards/blackrock/brawl.py
+++ fireplace/cards/blackrock/brawl.py
@@ -20,9 +20,19 @@ class TBA01_6:
 ##
 # Minions
 
+# Dragonkin Hatcher
+class BRMC_84:
+	play = Summon(CONTROLLER, "BRMA09_2Ht") * 2
+
+
 # Lucifron
 class BRMC_85:
 	play = Buff(ALL_MINIONS - SELF, "CS2_063e")
+
+
+# Atramedes
+class BRMC_86:
+	events = Play(OPPONENT).on(Buff(SELF, "BRMC_86e"))
 
 
 # Moira Bronzebeard
@@ -30,9 +40,19 @@ class BRMC_87:
 	deathrattle = Summon(CONTROLLER, "BRM_028")
 
 
+# Drakonid Slayer
+class BRMC_88:
+	events = Attack(SELF).on(Hit(TARGET_ADJACENT, Attr(SELF, GameTag.ATK)))
+
+
 # Son of the Flame
 class BRMC_91:
 	play = Hit(TARGET, 6)
+
+
+# Coren Direbrew
+class BRMC_92:
+	play = Give(CONTROLLER, "EX1_407")
 
 
 # Golemagg
@@ -45,6 +65,19 @@ class BRMC_96:
 	events = OWN_TURN_BEGIN.on(Summon(CONTROLLER, RandomMinion(rarity=Rarity.LEGENDARY)))
 
 
+# Razorgore
+class BRMC_98:
+	events = OWN_TURN_BEGIN.on(Buff(FRIENDLY_MINIONS, "BRMC_98e"))
+
+
 # Garr
 class BRMC_99:
 	events = SELF_DAMAGE.on(Summon(CONTROLLER, "BRMC_99e"))
+
+
+##
+# Spells
+
+# Open the Gates
+class BRMC_83:
+	play = Summon(CONTROLLER, "BRMA09_2Ht") * 7
