58b874d16df6b1b6b4fe07f4c044fc19b5db25b2
Jerome Leclanche <jerome@leclan.ch>
2015-07-29 13:49:44 +0200

    Fix Burrowing Mine trigger

--- fireplace/cards/gvg/warrior.py
+++ fireplace/cards/gvg/warrior.py
@@ -20,7 +20,7 @@ class GVG_056:
 
 # Burrowing Mine
 class GVG_056t:
-	play = Hit(FRIENDLY_HERO, 10), Draw(CONTROLLER)
+	events = Draw(CONTROLLER, SELF).on(Hit(FRIENDLY_HERO, 10), Draw(CONTROLLER))
 
 
 ##
