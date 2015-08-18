9201e9c433930da8fd0bfb13eadbc249469e4d84
Jerome Leclanche <jerome@leclan.ch>
2015-08-18 09:21:21 +0200

    Implement Mage cards for The Grand Tournament

--- fireplace/cards/tourney/mage.py
+++ fireplace/cards/tourney/mage.py
@@ -2,6 +2,42 @@ from ..utils import *
 
 
 ##
+# Minions
+
+# Dalaran Aspirant
+class AT_006:
+	inspire = Buff(SELF, "AT_006e")
+
+
+# Spellslinger
+class AT_007:
+	play = Give(ALL_PLAYERS, RandomSpell())
+
+
+# Rhonin
+class AT_009:
+	deathrattle = Give(CONTROLLER, "EX1_277") * 3
+
+
+##
+# Spells
+
+# Flame Lance
+class AT_001:
+	play = Hit(TARGET, 8)
+
+
+# Arcane Blast
+class AT_004:
+	play = Hit(TARGET, 2)
+
+
+# Polymorph: Boar
+class AT_005:
+	play = Morph(TARGET, "AT_005t")
+
+
+##
 # Secrets
 
 # Effigy
