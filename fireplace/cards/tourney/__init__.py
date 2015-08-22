a9ef1ba3cbe6463c4c104f328ee058046a331be4
Jerome Leclanche <jerome@leclan.ch>
2015-08-23 00:32:51 +0200

    Implement Rogue cards for The Grand Tournament

--- fireplace/cards/tourney/__init__.py
+++ fireplace/cards/tourney/__init__.py
@@ -1,6 +1,7 @@
 from .hunter import *
 from .mage import *
 from .priest import *
+from .rogue import *
 from .shaman import *
 from .neutral_common import *
 from .neutral_epic import *
---
+++ fireplace/cards/tourney/rogue.py
@@ -0,0 +1,50 @@
+from ..utils import *
+
+
+##
+# Minions
+
+# Shado-Pan Rider
+class AT_028:
+	combo = Buff(SELF, "AT_028e")
+
+
+# Buccaneer
+class AT_029:
+	events = Summon(FRIENDLY_WEAPON).on(Buff(Summon.Args.TARGETS, "AT_029e"))
+
+
+# Undercity Valiant
+class AT_030:
+	combo = Hit(TARGET, 1)
+
+
+# Cutpurse
+class AT_031:
+	events = Attack(SELF, HERO).on(Give(CONTROLLER, "GAME_005"))
+
+
+# Shady Dealer
+class AT_032:
+	play = Find(FRIENDLY_MINIONS + PIRATE) & Buff(SELF, "AT_032e")
+
+
+# Anub'arak
+class AT_036:
+	deathrattle = Bounce(SELF), Summon(CONTROLLER, "AT_036t")
+
+
+##
+# Spells
+
+# Burgle
+class AT_033:
+	play = Give(CONTROLLER, RandomCollectible(class_card=Attr(TARGET, GameTag.CLASS))) * 2
+
+
+# Beneath the Grounds
+class AT_035:
+	play = Shuffle(OPPONENT, "AT_035t") * 3
+
+class AT_035t:
+	in_hand = Summon(OPPONENT, "AT_036t")
