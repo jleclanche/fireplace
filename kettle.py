bffbb8c83f6aa0d718afa7ada73d6979400ab110
Jerome Leclanche <jerome@leclan.ch>
2015-08-24 06:44:46 +0200

    Implement KettleManager and move the game logic there

--- kettle.py
+++ kettle.py
@@ -9,7 +9,6 @@ from argparse import ArgumentParser
 from fireplace.game import Game
 from fireplace.entity import Entity
 from fireplace.player import Player
-from fireplace.enums import GameTag
 
 
 logging.basicConfig(level=logging.DEBUG)
@@ -21,7 +20,45 @@ DEBUG = KettleLogger.debug
 
 
 class KettleManager:
-	pass
+	def __init__(self, game):
+		self.game = game
+		self.queued_data = []
+
+	def action(self, type, args):
+		pass
+
+	def action_end(self, type, args):
+		pass
+
+	def new_entity(self, entity):
+		payload = self.full_entity(entity)
+		self.queued_data.append(payload)
+
+	def start_game(self):
+		self.queued_data.append(self.game_entity(self.game))
+		self.game.start()
+
+	def game_entity(self, game):
+		return {
+			"Type": "GameEntity",
+			"GameEntity": {
+				"EntityID": game.manager.id,
+				"Tags": Kettle._serialize_tags(game.tags),
+			}
+		}
+
+	def full_entity(self, entity):
+		if isinstance(entity, Player):
+			type = "Player"
+		else:
+			type = "FullEntity"
+		return {
+			"Type": type,
+			type: {
+				"EntityID": entity.manager.id,
+				"Tags": Kettle._serialize_tags(entity.tags),
+			}
+		}
 
 
 class Kettle(socketserver.BaseRequestHandler):
@@ -34,18 +71,19 @@ class Kettle(socketserver.BaseRequestHandler):
 		query_type = data["Type"]
 		payload = data[query_type]
 		DEBUG("Got payload %r", payload)
-		if query_type == "Init":
-			response = self.init_game(payload)
+		if query_type == "CreateGame":
+			manager = self.create_game(payload)
 		else:
 			raise NotImplementedError
 
-		serialized = json.dumps(response).encode("utf-8")
+		serialized = json.dumps(manager.queued_data).encode("utf-8")
+		manager.queued_data = []
 		response_payload = struct.pack("<i", len(serialized)) + serialized
 		DEBUG("Sending %r" % (response_payload))
 		self.request.sendall(response_payload)
-		self.manager = KettleManager()
 
-	def _serialize_tags(self, tags):
+	@staticmethod
+	def _serialize_tags(tags):
 		ret = {}
 		for k, v in tags.items():
 			if not v:
@@ -62,7 +100,7 @@ class Kettle(socketserver.BaseRequestHandler):
 			ret[int(k)] = v
 		return ret
 
-	def init_game(self, payload):
+	def create_game(self, payload):
 		self.game_id = payload["GameID"]
 		player_data = payload["Players"]
 		players = []
@@ -75,46 +113,12 @@ class Kettle(socketserver.BaseRequestHandler):
 			players.append(p)
 
 		INFO("Initializing a Kettle game with players=%r", players)
-		self.game = Game(players=players)
-		self.game.start()
+		game = Game(players=players)
+		manager = KettleManager(game)
+		game.manager.register(manager)
+		manager.start_game()
 
-		ret = []
-		ret.append(self.game_entity(self.game))
-		for player in self.game.players:
-			ret.append(self.player_entity(player))
-
-		for entity in self.game.all_entities:
-			ret.append(self.full_entity(entity))
-
-		return ret
-
-	def game_entity(self, game):
-		return {
-			"Type": "GameEntity",
-			"GameEntity": {
-				"EntityID": game.manager.id,
-				"Tags": self._serialize_tags(game.tags),
-			}
-		}
-
-	def player_entity(self, player):
-		return {
-			"Type": "Player",
-			"Player": {
-				"EntityID": player.manager.id,
-				"PlayerID": player.manager.id + 1,
-				"Tags": self._serialize_tags(player.tags),
-			}
-		}
-
-	def full_entity(self, card):
-		return {
-			"Type": "FullEntity",
-			"FullEntity": {
-				"EntityID": card.manager.id,
-				"Tags": self._serialize_tags(card.tags),
-			}
-		}
+		return manager
 
 
 def main():
