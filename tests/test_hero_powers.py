43a62e59836532005210e54e30f8ba00bfc5b8f2
Jerome Leclanche <jerome@leclan.ch>
2015-08-29 18:09:38 +0200

    Move class heroes tests to test_hero_powers.py

---
+++ tests/test_hero_powers.py
@@ -0,0 +1,97 @@
+from utils import *
+
+
+def test_mage():
+	game = prepare_game(MAGE, MAGE)
+	assert game.player1.hero.id is MAGE
+	assert game.player1.hero.health == 30
+	assert game.player1.opponent.hero.health == 30
+	assert game.player1.times_hero_power_used_this_game == 0
+	assert game.player1.hero.power.controller is game.player1
+	assert game.player2.hero.power.controller is game.player2
+
+	# Fireblast the opponent hero
+	game.player1.hero.power.use(target=game.player2.hero)
+	assert game.player1.hero.health == 30
+	assert game.player1.opponent.hero.health == 29
+	assert game.player1.times_hero_power_used_this_game == 1
+	assert not game.player1.hero.power.is_usable()
+
+
+def test_paladin():
+	game = prepare_game(PALADIN, PALADIN)
+	assert game.current_player.hero.id is PALADIN
+
+	game.current_player.hero.power.use()
+	assert len(game.board) == 1
+	assert len(game.current_player.field) == 1
+	assert game.current_player.field[0].id == "CS2_101t"
+
+	# ensure that hero power cannot be used on full board
+	game.end_turn(); game.end_turn()
+	assert game.player1.hero.power.is_usable()
+	for i in range(6):
+		game.player1.give(WISP).play()
+	assert len(game.player1.field) == 7
+	assert not game.player1.hero.power.is_usable()
+
+
+def test_priest():
+	game = prepare_game(PRIEST, PRIEST)
+	assert game.player1.hero.id is PRIEST
+	# Heal self
+	assert game.player1.hero.health == 30
+	game.player1.hero.power.use(target=game.player1.hero)
+	assert game.player1.hero.health == 30
+
+	game.end_turn(); game.end_turn()
+	game.player1.give(MOONFIRE).play(target=game.player1.hero)
+	assert game.player1.hero.health == 29
+	game.player1.hero.power.use(target=game.player1.hero)
+	assert game.player1.hero.health == 30
+	assert not game.player1.hero.power.is_usable()
+
+
+def test_shaman():
+	game = prepare_game(SHAMAN, SHAMAN)
+	assert game.player1.hero.id is SHAMAN
+	assert len(game.player1.hero.power.data.entourage) == 4
+
+	# use hero power four times
+	for i in range(4):
+		assert len(game.player1.field) == i
+		assert game.player1.hero.power.is_usable()
+		game.player1.hero.power.use()
+		assert len(game.player1.field) == i + 1
+		assert game.player1.field[-1].id in game.player1.hero.power.data.entourage
+		game.end_turn(); game.end_turn()
+
+	# ensure hero power can only be used again after a totem was destroyed
+	assert not game.player1.hero.power.is_usable()
+	game.player1.field[0].destroy()
+	assert game.player1.hero.power.is_usable()
+
+	# ensure that hero power cannot be used on full board
+	for i in range(4):
+		game.player1.give(WISP).play()
+	assert len(game.player1.field) == 7
+	assert not game.player1.hero.power.is_usable()
+
+
+def test_warlock():
+	game = prepare_game(WARLOCK, WARLOCK)
+	game.player1.discard_hand()
+	assert not game.player1.hero.power.targets
+	assert game.player1.hero.power.is_usable()
+	game.player1.hero.power.use()
+	assert len(game.player1.hand) == 1
+	assert game.player1.hero.health == 28
+
+	sacpact = game.current_player.give("NEW1_003")
+	assert not sacpact.is_playable()
+	flameimp = game.current_player.give("EX1_319")
+	flameimp.play()
+	assert game.current_player.hero.health == 25
+	assert sacpact.is_playable()
+	sacpact.play(target=flameimp)
+	assert game.current_player.hero.health == 30
