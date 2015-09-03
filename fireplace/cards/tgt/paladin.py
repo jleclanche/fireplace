b10e4967082d0f4c7da89bfd910982086fbf7817
Jerome Leclanche <jerome@leclan.ch>
2015-09-03 09:08:06 +0200

    Implement more TGT Paladin cards

--- fireplace/cards/tgt/paladin.py
+++ fireplace/cards/tgt/paladin.py
@@ -4,6 +4,11 @@ from ..utils import *
 ##
 # Minions
 
+# Warhorse Trainer
+class AT_075:
+	update = Refresh(FRIENDLY + ID("CS2_101t"), "AT_075e")
+
+
 # Murloc Knight
 class AT_076:
 	inspire = Summon(CONTROLLER, RandomMurloc())
@@ -12,6 +17,14 @@ class AT_076:
 # Eadric the Pure
 class AT_081:
 	play = Buff(ENEMY_MINIONS, "AT_081e")
+
+class AT_081e:
+	atk = lambda self, i: 1
+
+
+# Tuskarr Jouster
+class AT_104:
+	play = JOUST & Heal(FRIENDLY_HERO, 7)
 
 
 ##
@@ -30,3 +43,11 @@ class AT_073:
 	events = OWN_TURN_BEGIN.on(
 		Buff(FRIENDLY_MINIONS, "AT_073e"), Reveal(SELF)
 	)
+
+
+##
+# Weapons
+
+# Argent Lance
+class AT_077:
+	play = JOUST & Buff(SELF, "AT_077e")
