37ab58016e69993b5ab1d63c99d9afcf54bd95af
Jerome Leclanche <jerome@leclan.ch>
2015-09-03 09:25:20 +0200

    Implement more TGT Neutral Epics

--- fireplace/cards/tgt/neutral_epic.py
+++ fireplace/cards/tgt/neutral_epic.py
@@ -4,6 +4,31 @@ from ..utils import *
 ##
 # Minions
 
+# Twilight Guardian
+class AT_017:
+	play = HOLDING_DRAGON & Buff(SELF, "AT_017e")
+
+
+# Sideshow Spelleater
+class AT_098:
+	play = Summon(CONTROLLER, Copy(ENEMY_HERO_POWER))
+
+
 # Kodorider
 class AT_099:
 	inspire = Summon(CONTROLLER, "AT_099t")
+
+
+# Master of Ceremonies
+class AT_117:
+	play = Find(FRIENDLY_MINIONS + SPELLPOWER) & Buff(SELF, "AT_117e")
+
+
+# Frost Giant
+class AT_120:
+	cost = lambda self, i: i - self.controller.times_hero_power_used_this_game
+
+
+# Crowd Favorite
+class AT_121:
+	events = Play(CONTROLLER, BATTLECRY).on(Buff(SELF, "AT_121e"))
