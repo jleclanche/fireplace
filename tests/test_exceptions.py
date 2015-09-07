cff68731aac50206d34cf1c7a49952569fb1f6a9
Benedict Etzel <developer@beheh.de>
2015-09-07 11:50:29 +0200

    Improve exception tests

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -13,111 +13,122 @@ def test_concede():
 
 
 def test_play_on_wrong_turn():
+	game = prepare_game()
+	wisp = game.player2.give(WISP)
 	with pytest.raises(InvalidAction):
-		game = prepare_game()
-		wisp = game.player2.give(WISP)
 		wisp.play()
 
 
 def test_play_card_in_deck():
+	game = prepare_game()
+	wisp = game.player1.give(WISP)
+	wisp.shuffle_into_deck()
 	with pytest.raises(InvalidAction):
-		game = prepare_game()
-		wisp = game.player1.give(WISP)
-		wisp.shuffle_into_deck()
 		wisp.play()
 
 
-def test_play_double():
+def test_play_twice():
+	game = prepare_game()
+	wisp = game.player1.give(WISP)
+	wisp.play()
 	with pytest.raises(InvalidAction):
-		game = prepare_game()
-		wisp = game.player1.give(WISP)
-		wisp.play()
 		wisp.play()
 
 
 def test_play_without_target():
+	game = prepare_game()
+	game.player1.summon(WISP)
+	abusive = game.player1.give("CS2_188")
 	with pytest.raises(InvalidAction):
-		game = prepare_game()
-		game.player1.summon(WISP)
-		abusive = game.player1.give("CS2_188")
 		abusive.play()
 
 
 def test_play_with_invalid_target():
+	game = prepare_game()
+	game.player1.summon(WISP)
+	abusive = game.player1.give("CS2_188")
+	assert game.player1.hero not in abusive.targets
 	with pytest.raises(InvalidAction):
-		game = prepare_game()
-		game.player1.summon(WISP)
-		abusive = game.player1.give("CS2_188")
-		assert game.player1.hero not in abusive.targets
 		abusive.play(target=game.player1.hero)
 
 
 def test_play_choose_without_choice():
+	game = prepare_game()
+	powerofwild = game.player1.give("EX1_160")
 	with pytest.raises(InvalidAction):
-		game = prepare_game()
-		powerofwild = game.player1.give("EX1_160")
 		powerofwild.play()
 
 
 def test_attack_without_charge():
+	game = prepare_game()
+	wisp = game.player1.give(WISP)
 	with pytest.raises(InvalidAction):
-		game = prepare_game()
-		wisp = game.player1.give(WISP)
 		wisp.attack(game.player2.hero)
 
 
 def test_attack_on_wrong_turn():
-	with pytest.raises(InvalidAction):
-		game = prepare_game()
-		wisp = game.player1.give(WISP)
-		wisp.play()
-		game.end_turn()
+	game = prepare_game()
+	wisp = game.player1.give(WISP)
+	wisp.play()
+	game.end_turn()
 
+	with pytest.raises(InvalidAction):
 		wisp.attack(game.player2.hero)
 
 
-def test_attack_dead_minion():
-	with pytest.raises(InvalidAction):
-		game = prepare_game()
-		wisp = game.player1.give(WISP)
-		wisp.play()
-		game.end_turn()
+def test_attack_with_dead_minion():
+	game = prepare_game()
+	wisp = game.player1.give(WISP)
+	wisp.play()
+	game.end_turn(); game.end_turn()
 
+	game.player1.give(MOONFIRE).play(target=wisp)
+	with pytest.raises(InvalidAction):
 		wisp.attack(game.player2.hero)
 
 
 def test_attack_invalid_target():
+	game = prepare_game()
+	wisp = game.player1.give(WISP)
+	game.end_turn(); game.end_turn()
+
+	assert game.player1.hero not in wisp.targets
 	with pytest.raises(InvalidAction):
-		game = prepare_game()
-		wisp = game.player1.give(WISP)
-		game.end_turn(); game.end_turn()
-		assert game.player1.hero not in wisp.targets
 		wisp.attack(game.player1.hero)
 
 
 def test_attack_without_weapon():
+	game = prepare_game()
 	with pytest.raises(InvalidAction):
-		game = prepare_game()
 		game.player1.hero.attack(game.player2.hero)
 
 
 def test_attack_own_minion():
+	game = prepare_game()
+	wisp = game.player1.give(WISP)
+	wisp.play()
+	axe = game.player1.give("CS2_106")
+	axe.play()
 	with pytest.raises(InvalidAction):
-		game = prepare_game()
-		wisp = game.player1.give(WISP)
-		wisp.play()
 		game.player1.hero.attack(wisp)
 
 
 def test_hero_power_on_wrong_turn():
-	with pytest.raises(InvalidAction):
-		game = prepare_game(WARRIOR, WARRIOR)
-		game.end_turn()
+	game = prepare_game(WARRIOR, WARRIOR)
+	game.end_turn()
 
+	with pytest.raises(InvalidAction):
+		game.player1.hero.power.use()
+
+
+def test_hero_power_twice():
+	game = prepare_game(WARRIOR, WARRIOR)
+	game.player1.hero.power.use()
+	with pytest.raises(InvalidAction):
 		game.player1.hero.power.use()
 
 
 def test_hero_power_without_target():
+	game = prepare_game(MAGE, MAGE)
 	with pytest.raises(InvalidAction):
-		game = prepare_game(MAGE, MAGE)
 		game.player1.hero.power.use()
