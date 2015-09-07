a71aa80c171acbd6b7139d162e455ac11cf397d3
Jerome Leclanche <jerome@leclan.ch>
2015-09-07 06:16:28 +0200

    Use DSL imports for test_selector

--- tests/test_selector.py
+++ tests/test_selector.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from fireplace.dsl.selector import Selector
+from fireplace.dsl import *
 from utils import *
 
 
@@ -7,11 +7,10 @@ def test_selector():
 	game = prepare_game()
 	game.player1.discard_hand()
 	alex = game.player1.give("EX1_561")
-	selector = Selector(Race.PIRATE) | Selector(Race.DRAGON) + Selector(CardType.MINION)
+	selector = PIRATE | DRAGON + MINION
 	assert len(selector.eval(game.player1.hand, game.player1)) >= 1
 
-	in_hand = Selector(Zone.HAND)
-	selector = in_hand + Selector(Race.DRAGON) + Selector(Affiliation.FRIENDLY)
+	selector = IN_HAND + DRAGON + FRIENDLY
 	targets = selector.eval(game, game.player1)
 	assert len(targets) == 1
 	assert targets[0] == alex
@@ -21,7 +20,7 @@ def test_empty_selector():
 	game = prepare_game()
 	game.player1.discard_hand()
 	game.player2.discard_hand()
-	selector = Selector(Zone.HAND)
+	selector = IN_HAND
 
 	targets = selector.eval(game.player1.hand, game.player1)
 	assert not targets
