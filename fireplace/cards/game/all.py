d1f4c3f8b09e5fc808580d53d4b21db523ac9127
Jerome Leclanche <jerome@leclan.ch>
2015-11-08 02:53:11 +0200

    Implement Hero Powers from "Who's The Boss Now?" Tavern Brawl

--- fireplace/cards/game/all.py
+++ fireplace/cards/game/all.py
@@ -37,3 +37,66 @@ class TB_MechWar_Boss2_HeroPower:
 # Mysterious Pilot
 class TB_Pilot1:
 	deathrattle = Summon(CONTROLLER, RandomMinion(cost=Attr(SELF, GameTag.COST)))
+
+
+##
+# Tavern Brawl: Who's The Boss Now?
+
+# Pile On!!!
+class BRMA01_2H_2_TB:
+	activate = (
+		Summon(CONTROLLER, RANDOM(CONTROLLER_DECK + MINION)),
+		Summon(OPPONENT, RANDOM(OPPONENT_DECK + MINION))
+	)
+
+
+# Jeering Crowd
+class BRMA02_2_2_TB:
+	activate = Summon(CONTROLLER, "BRMA02_2t")
+
+# Jeering Crowd (Unused)
+class BRMA02_2_2c_TB:
+	play = Summon(CONTROLLER, "BRMA02_2t")
+
+
+# The Majordomo
+class BRMA06_2H_TB:
+	activate = Summon(CONTROLLER, "BRMA06_4H")
+
+
+# ME SMASH!
+class BRMA07_2_2_TB:
+	activate = Destroy(RANDOM_ENEMY_MINION)
+
+# ME SMASH! (Unused)
+class BRMA07_2_2c_TB:
+	play = Destroy(RANDOM_ENEMY_MINION)
+
+
+# Activate!
+class BRMA14_10H_TB:
+	Summon(CONTROLLER, RandomEntourage())
+
+
+# Wild Magic
+class BRMA13_4_2_TB:
+	activate = Give(CONTROLLER, RandomSpell(card_class=Attr(ENEMY_HERO, GameTag.CLASS)))
+
+
+# Web Wrap
+class NAX3_02_TB:
+	activate = Bounce(RANDOM_ENEMY_MINION)
+
+
+# Harvest
+class NAX8_02H_TB:
+	activate = Draw(CONTROLLER), GainMana(CONTROLLER, 1)
+
+
+# Decimate
+class NAX12_02H_2_TB:
+	activate = Buff(ENEMY_MINIONS, "NAX12_02e")
+
+# Decimate (Unused)
+class NAX12_02H_2c_TB:
+	play = Buff(ENEMY_MINIONS, "NAX12_02e")
