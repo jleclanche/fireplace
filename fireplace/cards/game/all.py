b04916b691ac1a9621eb998adf4de5cf78cc4b41
Jerome Leclanche <jerome@leclan.ch>
2015-12-13 07:23:07 +0200

    Implement Luck of the Coin and Coin's Vengeance
    Who the hell knows.

--- fireplace/cards/game/all.py
+++ fireplace/cards/game/all.py
@@ -4,6 +4,17 @@ GAME set and other special cards
 from ..utils import *
 
 
+# Luck of the Coin
+GAME_001 = buff(health=3)
+
+
+# Coin's Vengeance
+class GAME_003:
+	events = Play(CONTROLLER, MINION).on(Buff(Play.CARD, "GAME_003e"), Destroy(SELF))
+
+GAME_003e = buff(+1, +1)
+
+
 # AFK
 class GAME_004:
 	update = Refresh(CONTROLLER, {GameTag.TIMEOUT: 10})
