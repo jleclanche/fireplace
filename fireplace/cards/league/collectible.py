b003661baec68e0f49a1dd8ff65496623fd38ad0
Jerome Leclanche <jerome@leclan.ch>
2015-12-16 11:56:00 +0200

    Implement various Discover cards

--- fireplace/cards/league/collectible.py
+++ fireplace/cards/league/collectible.py
@@ -9,14 +9,40 @@ class LOE_003:
 	play = Discover(CONTROLLER, RandomSpell())
 
 
+# Museum Curator
+class LOE_006:
+	play = Discover(CONTROLLER, RandomCollectible(deathrattle=True))
+
+
 # Obsidian Destroyer
 class LOE_009:
 	events = OWN_TURN_END.on(Summon(CONTROLLER, "LOE_009t"))
 
 
+# Dark Peddler
+class LOE_023:
+	play = Discover(CONTROLLER, RandomCollectible(cost=1))
+
+
+# Jeweled Scarab
+class LOE_029:
+	play = Discover(CONTROLLER, RandomCollectible(cost=3))
+
+
+# Gorillabot A-3
+class LOE_039:
+	powered_up = Find(FRIENDLY_MINIONS + MECH)
+	play = powered_up & Discover(CONTROLLER, RandomMech())
+
+
 # Huge Toad
 class LOE_046:
 	deathrattle = Hit(RANDOM_ENEMY_CHARACTER, 1)
+
+
+# Tomb Spider
+class LOE_047:
+	play = Discover(CONTROLLER, RandomBeast())
 
 
 # Fossilized Devilsaur
@@ -70,6 +96,12 @@ LOE_113e = buff(+2, +2)
 # Raven Idol
 class LOE_115:
 	choose = ("LOE_115a", "LOE_115b")
+
+class LOE_115a:
+	play = Discover(CONTROLLER, RandomMinion())
+
+class LOE_115b:
+	play = Discover(CONTROLLER, RandomSpell())
 
 
 ##
