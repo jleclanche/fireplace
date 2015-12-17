34080ef852cefa1b7e51dbd3608387b0b7fd0fe8
Jerome Leclanche <jerome@leclan.ch>
2015-12-17 04:41:05 +0200

    Implement Gift Exchange brawl cards

--- fireplace/cards/game/all.py
+++ fireplace/cards/game/all.py
@@ -64,6 +64,33 @@ class TB_Pilot1:
 
 
 ##
+# Tavern Brawl: Gift Exchange
+
+# Hardpacked Snowballs
+class TB_GiftExchange_Snowball:
+	play = Bounce(RANDOM_ENEMY_MINION) * 3
+
+
+# Winter's Veil Gift
+class TB_GiftExchange_Treasure:
+	deathrattle = Give(CURRENT_PLAYER, "TB_GiftExchange_Treasure_Spell")
+
+
+# Stolen Winter's Veil Gift
+class TB_GiftExchange_Treasure_Spell:
+	# Surely none of this even sort of works.
+	RandomGift = RandomCollectible(
+		COST >= 5,
+		CLASS_CARD=Attr(Controller(OWNER), GameTag.CLASS)
+	)
+	play = Discover(RandomGift).then(Buff(Discover.CARDS, "TB_GiftExchange_Enchantment"))
+
+
+# Cheap Gift
+TB_GiftExchange_Enchantment = buff(cost=-5)
+
+
+##
 # Tavern Brawl: Who's The Boss Now?
 
 # Pile On!!!
