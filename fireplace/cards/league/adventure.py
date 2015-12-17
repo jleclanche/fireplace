6917331e83a33a6aa25f050f86fd837a4415efd7
Jerome Leclanche <jerome@leclan.ch>
2015-12-17 21:36:25 +0200

    Implement Zinaar LOE adventure cards

--- fireplace/cards/league/adventure.py
+++ fireplace/cards/league/adventure.py
@@ -10,6 +10,49 @@ class LOEA16_12:
 
 
 ##
+# Zinaar
+
+RandomWish = RandomID("LOEA02_03", "LOEA02_04", "LOEA02_05", "LOEA02_06", "LOEA02_10")
+
+# Djinn's Intuition
+class LOEA02_02:
+	activate = Draw(CONTROLLER), Give(OPPONENT, RandomWish)
+
+class LOEA02_02h:
+	activate = Draw(CONTROLLER), GainMana(CONTROLLER, 1), Give(OPPONENT, RandomWish)
+
+
+# Wish for Power
+class LOEA02_03:
+	play = Discover(RandomSpell())
+
+
+# Wish for Valor
+class LOEA02_04:
+	play = Discover(RandomCollectible(cost=4))
+
+
+# Wish for Glory
+class LOEA02_05:
+	play = Discover(RandomMinion())
+
+
+# Wish for More Wishes
+class LOEA02_06:
+	play = Discover(RandomWish)
+
+
+# Wish for Companionship
+class LOEA02_10:
+	play = Discover(RandomID("NEW1_032", "NEW1_033", "NEW1_034"))
+
+
+# Leokk (Unused)
+class LOEA02_10a:
+	update = Refresh(FRIENDLY_MINIONS - SELF, buff="NEW1_033o")
+
+
+##
 # Temple Escape events
 
 # Pit of Spikes
