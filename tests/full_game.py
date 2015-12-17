a1ab0d7b647eed9a406173b6dde368956971f8d1
Jerome Leclanche <jerome@leclan.ch>
2015-12-17 05:06:47 +0200

    Allow running more than one game in full_game.py

--- tests/full_game.py
+++ tests/full_game.py
@@ -75,7 +75,15 @@ def test_full_game():
 
 
 def main():
-	test_full_game()
+	if len(sys.argv) > 1:
+		numgames = sys.argv[1]
+		if not numgames.isdigit():
+			sys.stderr.write("Usage: %s [NUMGAMES]\n" % (sys.argv[0]))
+			exit(1)
+		for i in range(int(numgames)):
+			test_full_game()
+	else:
+		test_full_game()
 
 
 if __name__ == "__main__":
