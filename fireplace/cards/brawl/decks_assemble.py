51d1701bbc8b25bfd7b4f70c83fec7a46d965bef
Jerome Leclanche <jerome@leclan.ch>
2016-03-10 10:46:54 +0200

    Implement Decks Assemble brawl rules on the Deckbuilding Enchant

--- fireplace/cards/brawl/decks_assemble.py
+++ fireplace/cards/brawl/decks_assemble.py
@@ -5,6 +5,14 @@ Decks Assemble
 from ..utils import *
 
 
+# Deckbuilding Enchant
+class TB_010:
+	events = (
+		OWN_TURN_BEGIN.on(Discover(CONTROLLER, RandomCollectible())),
+		Play(CONTROLLER).on(Shuffle(CONTROLLER, Copy(Play.CARD))),
+		OWN_TURN_END.on(Shuffle(CONTROLLER, FRIENDLY_HAND))
+	)
+
 # Tarnished Coin
 class TB_011:
 	play = ManaThisTurn(CONTROLLER, 1)
