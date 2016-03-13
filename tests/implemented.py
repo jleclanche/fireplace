d400f583c1973147bbba597c1fc2c6b9c24cc13c
Jerome Leclanche <jerome@leclan.ch>
2016-03-13 09:16:24 +0200

    implemented.py: Fix a trailing comma

--- tests/implemented.py
+++ tests/implemented.py
@@ -42,7 +42,7 @@ DUMMY_CARDS = (
 	# Dynamic buffs set by their parent
 	"CS2_236e",  # Divine Spirit
 	"EX1_304e",  # Consume (Void Terror)
-	"LOE_030e"  # Hollow (Unused)
+	"LOE_030e",  # Hollow (Unused)
 	"NEW1_018e",  # Treasure Crazed (Bloodsail Raider)
 )
 
