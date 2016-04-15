bfd741a63286a40ed4566289145cfc7683fc9534
Jerome Leclanche <jerome@leclan.ch>
2016-04-15 23:19:19 +0300

    Remove positional arguments support from RandomCardPicker
    They are more problematic than anything else at the moment.

--- fireplace/dsl/random_picker.py
+++ fireplace/dsl/random_picker.py
@@ -54,16 +54,16 @@ class RandomCardPicker(LazyValue):
 		return [source.controller.card(card, source=source) for card in ret]
 
 
-RandomCard = lambda *a, **kw: RandomCardPicker(*a, **kw)
-RandomCollectible = lambda *a, **kw: RandomCardPicker(*a, collectible=True, **kw)
-RandomMinion = lambda *a, **kw: RandomCollectible(*a, type=CardType.MINION, **kw)
-RandomBeast = lambda *a, **kw: RandomMinion(*a, race=Race.BEAST)
-RandomMech = lambda *a, **kw: RandomMinion(*a, race=Race.MECHANICAL)
-RandomMurloc = lambda *a, **kw: RandomMinion(*a, race=Race.MURLOC)
-RandomSpell = lambda *a, **kw: RandomCollectible(*a, type=CardType.SPELL, **kw)
-RandomTotem = lambda *a, **kw: RandomCardPicker(*a, race=Race.TOTEM)
-RandomWeapon = lambda *a, **kw: RandomCollectible(*a, type=CardType.WEAPON, **kw)
-RandomLegendaryMinion = lambda *a, **kw: RandomMinion(*a, rarity=Rarity.LEGENDARY, **kw)
+RandomCard = lambda **kw: RandomCardPicker(**kw)
+RandomCollectible = lambda **kw: RandomCardPicker(collectible=True, **kw)
+RandomMinion = lambda **kw: RandomCollectible(type=CardType.MINION, **kw)
+RandomBeast = lambda **kw: RandomMinion(race=Race.BEAST)
+RandomMech = lambda **kw: RandomMinion(race=Race.MECHANICAL)
+RandomMurloc = lambda **kw: RandomMinion(race=Race.MURLOC)
+RandomSpell = lambda **kw: RandomCollectible(type=CardType.SPELL, **kw)
+RandomTotem = lambda **kw: RandomCardPicker(race=Race.TOTEM)
+RandomWeapon = lambda **kw: RandomCollectible(type=CardType.WEAPON, **kw)
+RandomLegendaryMinion = lambda **kw: RandomMinion(rarity=Rarity.LEGENDARY, **kw)
 RandomSparePart = lambda: RandomCardPicker(spare_part=True)
 
 
