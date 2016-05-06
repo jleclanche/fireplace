ddba6236ac497fab5b438d0f9162b79f2333ec22
smallnamespace <smallnamespace@gmail.com>
2016-05-06 14:19:11 -0700

    Integrate pytest-benchmark, add selector and full_game tests.
    Closes #314

---
+++ tests/test_micro_bench.py
@@ -0,0 +1,24 @@
+from full_game import test_full_game
+from utils import *
+
+
+def run_selector(game, alex):
+	selector = PIRATE | DRAGON + MINION
+	assert len(selector.eval(game.player1.hand, game.player1)) >= 1
+
+	selector = IN_HAND + DRAGON + FRIENDLY
+	targets = selector.eval(game, game.player1)
+	assert len(targets) == 1
+	assert targets[0] == alex
+
+
+def test_selectors(benchmark):
+	game = prepare_game()
+	game.player1.discard_hand()
+	alex = game.player1.give("EX1_561")
+
+	benchmark(run_selector, game, alex)
+
+
+def test_fullgame(benchmark):
+	benchmark(test_full_game)
