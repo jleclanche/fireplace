0fbd7c2f68f9f751642fd0e618dfcb6726d79f44
Alexander Strada <astrada2023@gmail.com>
2016-05-15 22:45:01 -0500

    Implement Twilight Flamecaller, Anomalus, Faceless Summoner

--- fireplace/cards/wog/mage.py
+++ fireplace/cards/wog/mage.py
@@ -4,4 +4,16 @@ from ..utils import *
 ##
 # Minions
 
+class OG_083:
+	"Twilight Flamecaller"
+	play = Hit(ENEMY_MINIONS, 1)
 
+
+class OG_120:
+	"Anomalus"
+	deathrattle = Hit(ALL_MINIONS, 8)
+
+
+class OG_207:
+	"Faceless Summoner"
+	play = Summon(CONTROLLER, RandomMinion(cost=3))
