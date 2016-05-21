2cb2779bfe1ddfcd6651665276ed0a1d513c57de
Kevin Skusek <nightkev@gmail.com>
2016-05-20 23:27:11 -0700

    Implement Eternal Sentinel, Stormcrack and Hammer of Twilight

--- fireplace/cards/wog/shaman.py
+++ fireplace/cards/wog/shaman.py
@@ -11,6 +11,27 @@ class OG_023:
 OG_023t = buff(+1, +1)
 
 
+class OG_026:
+	"Eternal Sentinel"
+	play = UnlockOverload(CONTROLLER)
+
+
 class OG_209:
 	"Hallazeal the Ascended"
 	events = Damage(source=SPELL + FRIENDLY).on(Heal(FRIENDLY_HERO, Damage.AMOUNT))
+
+
+##
+# Spells
+
+class OG_206:
+	"Stormcrack"
+	play = Hit(TARGET, 4)
+
+
+##
+# Weapons
+
+class OG_031:
+	"Hammer of Twilight"
+	deathrattle = Summon(CONTROLLER, "OG_031a")
