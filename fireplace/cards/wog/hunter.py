10dea74d7f7946e9bab8c99b489793708845183c
Kevin Skusek <nightkev@gmail.com>
2016-05-22 23:55:03 -0700

    Implement Infest, On the Hunt, Call of the Wild

--- fireplace/cards/wog/hunter.py
+++ fireplace/cards/wog/hunter.py
@@ -14,3 +14,30 @@ class OG_292:
 	play = Buff(FRIENDLY_HAND + MINION + DEATHRATTLE, "OG_292e")
 
 OG_292e = buff(+1, +1)
+
+
+##
+# Spells
+
+class OG_045:
+	"Infest"
+	play = Buff(FRIENDLY_MINIONS, "OG_045a")
+
+class OG_045a:
+	"Nerubian Spores"
+	deathrattle = Give(CONTROLLER, RandomBeast())
+	tags = {GameTag.DEATHRATTLE: True}
+
+
+class OG_061:
+	"On the Hunt"
+	play = Hit(TARGET, 1), Summon(CONTROLLER, "OG_061t")
+
+
+class OG_211:
+	"Call of the Wild"
+	play = (
+		Summon(CONTROLLER, "NEW1_034"),
+		Summon(CONTROLLER, "NEW1_033"),
+		Summon(CONTROLLER, "NEW1_032")
+	)
