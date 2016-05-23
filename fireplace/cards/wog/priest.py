a1d9e1ed4ac8b7542b6430f84b2ed9197d45d577
Kevin Skusek <nightkev@gmail.com>
2016-05-23 07:20:23 -0700

    Implement Darkshire Alchemist, Shifting Shade, Power Word: Tentacles

--- fireplace/cards/wog/priest.py
+++ fireplace/cards/wog/priest.py
@@ -4,4 +4,21 @@ from ..utils import *
 ##
 # Minions
 
+class OG_234:
+	"Darkshire Alchemist"
+	play = Heal(TARGET, 5)
 
+
+class OG_335:
+	"Shifting Shade"
+	deathrattle = Give(CONTROLLER, Copy(RANDOM(ENEMY_DECK)))
+
+
+##
+# Spells
+
+class OG_094:
+	"Power Word: Tentacles"
+	play = Buff(TARGET, "OG_094e")
+
+OG_094e = buff(+2, +6)
