ce2e5b0dc3ddafe931a902cb7aa24c3adbc246b7
Kevin Skusek <nightkev@gmail.com>
2016-05-25 13:21:30 -0700

    Implement corrupted Mukla, Hogger and Nat

--- fireplace/cards/wog/neutral_legendary.py
+++ fireplace/cards/wog/neutral_legendary.py
@@ -3,3 +3,17 @@ from ..utils import *
 
 ##
 # Minions
+
+class OG_122:
+	"Mukla, Tyrant of the Vale"
+	play = Give(CONTROLLER, "EX1_014t") * 2
+
+
+class OG_318:
+	"Hogger, Doom of Elwynn"
+	events = SELF_DAMAGE.on(Summon(CONTROLLER, "OG_318t"))
+
+
+class OG_338:
+	"Nat, the Darkfisher"
+	events = BeginTurn(OPPONENT).on(COINFLIP & Draw(OPPONENT))
