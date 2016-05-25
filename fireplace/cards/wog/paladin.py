e8831f22b6e31f38fc95e2c40e7bdf25ece11116
Kevin Skusek <nightkev@gmail.com>
2016-05-25 04:55:57 -0700

    Implement Selfless Hero and Rallying Blade

--- fireplace/cards/wog/paladin.py
+++ fireplace/cards/wog/paladin.py
@@ -4,7 +4,6 @@ from ..utils import *
 ##
 # Minions
 
-
 class OG_006:
 	"Vilefin Inquisitor"
 	play = Summon(CONTROLLER, "OG_006b")
@@ -12,3 +11,23 @@ class OG_006:
 class OG_006b:
 	"The Tidal Hand"
 	activate = Summon(CONTROLLER, "OG_006a")
+
+
+class OG_221:
+	"Selfless Hero"
+	deathrattle = GiveDivineShield(RANDOM_FRIENDLY_MINION)
+
+
+##
+# Spells
+
+
+
+##
+# Weapons
+
+class OG_222:
+	"Rallying Blade"
+	play = Buff(FRIENDLY_MINIONS + DIVINE_SHIELD, "OG_222e")
+
+OG_222e = buff(+1, +1)
