b7e38f3fc299d906ab81ab7826af96ea4769d066
Kevin Skusek <nightkev@gmail.com>
2016-05-25 10:20:49 -0700

    Implement Aberrant Berserker, Infested Tauren, and Spawn of N'Zoth

--- fireplace/cards/wog/neutral_common.py
+++ fireplace/cards/wog/neutral_common.py
@@ -4,6 +4,13 @@ from ..utils import *
 ##
 # Minions
 
+class OG_150:
+	"Aberrant Berserker"
+	enrage = Refresh(SELF, buff="OG_150e")
+
+OG_150e = buff(atk=2)
+
+
 class OG_151:
 	"Tentacle of N'Zoth"
 	deathrattle = Hit(ALL_MINIONS, 1)
@@ -19,6 +26,18 @@ class OG_158:
 	deathrattle = Buff(RANDOM_FRIENDLY_MINION, "OG_158e")
 
 OG_158e = buff(+1, +1)
+
+
+class OG_249:
+	"Infested Tauren"
+	deathrattle = Summon(CONTROLLER, "OG_249a")
+
+
+class OG_256:
+	"Spawn of N'Zoth"
+	deathrattle = Buff(FRIENDLY_MINIONS, "OG_256e")
+
+OG_256e = buff(+1, +1)
 
 
 class OG_323:
