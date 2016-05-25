79dd629be9b858fd7bc73e7d16aecbb25de0d5db
Kevin Skusek <nightkev@gmail.com>
2016-05-25 05:59:54 -0700

    Implement Bladed Cultist, Southsea Squidface, and Shadow Strike

--- fireplace/cards/wog/rogue.py
+++ fireplace/cards/wog/rogue.py
@@ -4,6 +4,19 @@ from ..utils import *
 ##
 # Minions
 
+class OG_070:
+	"Bladed Cultist"
+	combo = Buff(SELF, "OG_070e")
+
+OG_070e = buff(+1, +1)
+
+
+class OG_267:
+	"Southsea Squidface"
+	deathrattle = Buff(FRIENDLY_WEAPON, "OG_267e")
+
+OG_267e = buff(atk=2)
+
 
 ##
 # Spells
@@ -11,3 +24,8 @@ from ..utils import *
 class OG_073:
 	"Thistle Tea"
 	play = Draw(CONTROLLER).then(Give(CONTROLLER, Copy(Draw.CARD)) * 2)
+
+
+class OG_176:
+	"Shadow Strike"
+	play = Hit(TARGET, 5)
