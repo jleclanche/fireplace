c8921cf12418762c17d0b858ea2e134f292b2838
Kevin Skusek <nightkev@gmail.com>
2016-05-25 11:52:10 -0700

    Implement Twilight Summoner and Cyclopian Horror

--- fireplace/cards/wog/neutral_epic.py
+++ fireplace/cards/wog/neutral_epic.py
@@ -10,3 +10,15 @@ class OG_271:
 
 class OG_271e:
 	atk = lambda self, i: i * 2
+
+
+class OG_272:
+	"Twilight Summoner"
+	deathrattle = Summon(CONTROLLER, "OG_272t")
+
+
+class OG_337:
+	"Cyclopian Horror"
+	play = Buff(SELF, "OG_337e") * Count(ENEMY_MINIONS)
+
+OG_337e = buff(health=1)
