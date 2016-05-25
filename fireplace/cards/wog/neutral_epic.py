77ea739cabc05d6f96f88c05b754efb18b064d0a
Kevin Skusek <nightkev@gmail.com>
2016-05-25 11:22:29 -0700

    Update Silithid Swarmer and remove two duplicate Tentacle of N'Zoths

--- fireplace/cards/wog/neutral_epic.py
+++ fireplace/cards/wog/neutral_epic.py
@@ -4,11 +4,6 @@ from ..utils import *
 ##
 # Minions
 
-class OG_151:
-	"Tentacle of N'Zoth"
-	deathrattle = Hit(ALL_MINIONS, 1)
-
-
 class OG_271:
 	"Scaled Nightmare"
 	events = OWN_TURN_BEGIN.on(Buff(SELF, "OG_271e"))
