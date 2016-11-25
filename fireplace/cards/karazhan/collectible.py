593900489c7f68f89dd649c75c719d024fef3951
Yak Jun Xiang <snowypowers@gmail.com>
2016-11-25 11:42:34 +0800

    Add class definitions for Karazhan collectibles
    Added all class definitions as comments for karazhan. This allows easy tracking as to what needs to be implemented rather than running implemented.py

---
+++ fireplace/cards/karazhan/collectible.py
@@ -0,0 +1,174 @@
+from ..utils import *
+
+##
+# Minions
+
+# class KAR_005:
+# 	"Kindly Grandmother"
+
+
+# class KAR_006:
+# 	"Cloaked Huntress"
+
+
+# class KAR_009:
+# 	"Babbling Book"
+
+
+# class KAR_010:
+# 	"Nightbane Templar"
+
+
+# class KAR_021:
+# 	"Wicked Witchdoctor"
+
+
+# class KAR_029:
+# 	"Runic Egg"
+
+
+# class KAR_030a:
+# 	"Pantry Spider"
+
+
+# class KAR_033:
+# 	"Book Wyrm"
+
+
+# class KAR_035:
+# 	"Priest of the Feast"
+
+
+# class KAR_036:
+# 	"Arcane Anomaly"
+
+
+# class KAR_037:
+# 	"Avian Watcher"
+
+
+# class KAR_041:
+# 	"Moat Lurker"
+
+
+# class KAR_044:
+# 	"Moroes"
+
+
+# class KAR_057:
+# 	"Ivory Knight"
+
+
+# class KAR_061:
+# 	"The Curator"
+
+
+# class KAR_062:
+# 	"Netherspite Historian"
+
+
+# class KAR_065:
+# 	"Menagerie Warden"
+
+
+# class KAR_069:
+# 	"Swashburglar"
+
+
+# class KAR_070:
+# 	"Ethereal Peddler"
+
+
+# class KAR_089:
+# 	"Malchezaar's Imp"
+
+
+# class KAR_092:
+# 	"Medivh's Valet"
+
+
+# class KAR_094:
+# 	"Deadly Fork"
+
+
+# class KAR_095:
+# 	"Zoobot"
+
+
+# class KAR_096:
+# 	"Prince Malchezaar"
+
+
+# class KAR_097:
+# 	"Medivh, the Guardian"
+
+
+# class KAR_114:
+# 	"Barnes"
+
+
+# class KAR_114e:
+
+
+# class KAR_204:
+# 	"Onyx Bishop"
+
+
+# class KAR_205:
+# 	"Silverware Golem"
+
+
+# class KAR_702:
+# 	"Menagerie Magician"
+
+
+# class KAR_710:
+# 	"Arcanosmith"
+
+
+# class KAR_711:
+# 	"Arcane Giant"
+
+
+# class KAR_712:
+# 	"Violet Illusionist"
+
+##
+# Spells
+
+# class KAR_004:
+# 	"Cat Trick"
+
+# class KAR_013:
+# 	"Purify"
+
+# class KAR_025:
+# 	"Kara Kazham!"
+
+# class KAR_026:
+# 	"Protect the King!"
+
+# class KAR_073:
+# 	"Maelstrom Portal"
+
+# class KAR_075:
+# 	"Moonglade Portal"
+
+# class KAR_076:
+# 	"Firelands Portal"
+
+# class KAR_077:
+# 	"Silvermoon Portal"
+
+# class KAR_091:
+# 	"Ironforge Portal"
+
+##
+# Weapons
+
+# class KAR_028:
+# 	"Fool's Bane"
+
+# class KAR_063:
+# 	"Spirit Claws"
+
