a0ad7144c2c89d8ee622428056532e3c325dc352
Yak Jun Xiang <snowypowers@gmail.com>
2016-12-12 10:01:35 +0800

    Initialise Gangs cardlist

---
+++ fireplace/cards/gangs/mage.py
@@ -0,0 +1,35 @@
+from ..utils import *
+
+##
+# Minions
+
+#class CFM_066:
+#	"Kabal Lackey"
+
+#class CFM_660:
+#	"Manic Soulcaster"
+
+#class CFM_671:
+#	"Cryomancer"
+
+#class CFM_687:
+#	"Inkmaster Solia"
+
+#class CFM_760:
+#	"Kabal Crystal Runner"
+
+##
+# Spells
+
+#class CFM_021:
+#	"Freezing Potion"
+
+#class CFM_065:
+#	"Volcanic Potion"
+
+#class CFM_620:
+#	"Potion of Polymorph"
+
+#class CFM_623:
+#	"Greater Arcane Missiles"
+
