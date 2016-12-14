92d21257a977dc6f37232672500a8346ab4847f0
Yak Jun Xiang <snowypowers@gmail.com>
2016-12-14 18:12:31 +0800

    Implement "Virmen Sensei" with test
    CFM_816

--- tests/test_gangs.py
+++ tests/test_gangs.py
@@ -14,3 +14,22 @@ def test_celestial_dreamer():
 	assert dreamer.buffs
 	assert dreamer.atk == 5
 	assert dreamer. health == 5
+
+def test_virmen_sensei():
+	game = prepare_empty_game()
+	sensei1 = game.player1.give("CFM_816")
+	wisp = game.player1.give(WISP).play()
+	assert not sensei1.powered_up
+
+	sensei1.play()
+
+	beast = game.player1.give(CHICKEN).play()
+	sensei2 = game.player1.give("CFM_816")
+	assert sensei2.powered_up
+	game.player1.give(INNERVATE).play()
+	sensei2.play(target=beast)
+
+	assert beast.buffs
+	assert beast.atk == 3
+	assert beast.health == 3
+
