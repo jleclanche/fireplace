22b43e201a01058e22f15cd2477cf609f07b97af
Yak Jun Xiang <snowypowers@gmail.com>
2016-12-20 10:56:08 +0800

    Implement "Luckydo Buccaneer" with test
    CFM_342

--- fireplace/cards/gangs/rogue.py
+++ fireplace/cards/gangs/rogue.py
@@ -3,14 +3,15 @@ from ..utils import *
 ##
 # Minions
 
-#class CFM_342:
-#	"Luckydo Buccaneer"
+class CFM_342:
+	"Luckydo Buccaneer"
+	powered_up = Find(FRIENDLY_WEAPON + (ATK >= 3)) 
+	play = powered_up & Buff(SELF, "CFM_342e")
+
+CFM_342e = buff(+4, +4)
 
 #class CFM_634:
 #	"Lotus Assassin"
-
-#class CFM_636:
-#	"Shadow Rager"
 
 #class CFM_691:
 #	"Jade Swarmer"
