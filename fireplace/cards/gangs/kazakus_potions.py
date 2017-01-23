e9ec916eb3f97887935c637ffc091005b3a78ce8
Yak Jun Xiang <snowypowers@gmail.com>
2017-01-23 13:23:07 +0800

    Add list of kazakus potions

---
+++ fireplace/cards/gangs/kazakus_potions.py
@@ -0,0 +1,177 @@
+from ..utils import *
+
+
+class CFM_621t:
+	"Kazakus Potion"
+	# 1 Cost Potion
+	pass
+
+class CFM_621t2:
+	"Heart of Fire"
+	# 1 Cost: Deal 3
+	pass
+
+class CFM_621t3:
+	"Stonescale Oil"
+	# 1 Cost: Gain 4 Armor
+	pass
+
+class CFM_621t4:
+	"Felbloom"
+	# 1 Cost: 2 AOE
+	pass
+
+class CFM_621t5:
+	"Icecap"
+	# 1 Cost: Freeze 1
+	pass
+
+class CFM_621t6:
+	"Goldthorn"
+	# 1 Cost: +2 Health
+	pass
+
+class CFM_621t8:
+	"Kingsblood"
+	# 1 Cost: Draw 1
+	pass
+
+class CFM_621t9:
+	"Shadow Oil"
+	# 1 Cost: Add 1 Demon
+	pass
+
+class CFM_621t10:
+	"Netherbloom"
+	# 1 Cost: Summon 2/2
+
+class CFM_621t11:
+	"Lesser Potion"
+	# 1 Cost Selector
+	pass
+
+class CFM_621t12:
+	"Greater Potion"
+	# 5 Cost Selector
+	pass
+
+class CFM_621t13:
+	"Superior Potion"
+	# 10 Cost Selector
+	pass
+
+class CFM_621t14:
+	"Kazakus' Potion"
+	# 5 Cost Potion
+	pass
+
+class CFM_621t15:
+	"Kazakus' Potion"
+	# 10 Cost Potion
+	pass
+
+class CFM_621t16:
+	"Heart of Fire"
+	# 5 Cost: Deal 5 Dmg
+	pass
+
+class CFM_621t17:
+	"Stonescale Oil"
+	# 5 Cost: Gain 7 Armor
+	pass
+
+class CFM_621t18:
+	"Felbloom"
+	# 5 Cost: AOE 4
+	pass
+
+class CFM_621t19:
+	"Icecap"
+	# 5 Cost: Freeze 2
+	pass
+
+class CFM_621t20:
+	"Netherbloom"
+	# 5 Cost: Summon 5/5
+	pass
+
+class CFM_621t21:
+	"Mystic Wool"
+	# 5 Cost: Polymorph 1
+	pass
+
+class CFM_621t22:
+	"Kingsblood"
+	# 5 Cost: Draw 2
+	pass
+
+class CFM_621t23:
+	"Shadow Oil"
+	# 5 Cost: Add 2 Demons
+	pass
+
+class CFM_621t24:
+	"Goldthorn"
+	# 5 Cost: +4 Health
+	pass
+
+class CFM_621t25:
+	"Heart of Fire"
+	#10 Cost: Deal 10
+	pass
+
+class CFM_621t26:
+	"Stonescale Oil"
+	# 10 Cost: Gain 10 Armor
+	pass
+
+class CFM_621t27:
+	"Icecap"
+	# 10 Cost: Freeze 3
+	pass
+
+class CFM_621t28:
+	"Netherbloom"
+	# 10 Cost: Summon 8/8
+	pass
+
+class CFM_621t29:
+	"Mystic Wool"
+	# 10 Cost: Polymorph all
+	pass
+
+class CFM_621t30:
+	"Kingsblood"
+	# 10 Cost: Draw 3
+	pass
+
+class CFM_621t31:
+	"Shadow Oil"
+	# 10 Cost: Add 3 Demons
+	pass
+
+class CFM_621t32:
+	"Goldthorn"
+	# 10 Cost: +6 Health
+	pass
+
+class CFM_621t33:
+	"Felbloom"
+	# 10 Cost: 6 AOE
+	pass
+
+class CFM_621t37:
+	"Ichor of Undeath"
+	# 1 Cost: Revive 1
+	pass
+
+class CFM_621t38:
+	"Ichor of Undeath"
+	# 5 Cost: Revive 2
+	pass
+
+class CFM_621t39:
+	"Ichor of Undeath"
+	# 10 Cost: Revive 3
+	pass
+
