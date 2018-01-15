95633501ab75111c37d9155e98d24a7835ed2595
semitable <almak72@gmail.com>
2018-01-15 17:10:44 +0200

    Card implementation helper will find and print cards that seem similar

--- scripts/card_implementer.py
+++ scripts/card_implementer.py
@@ -1,4 +1,6 @@
+import string
 import sys
+import re
 
 if __name__ == "__main__":
 	sys.path.append("..")
@@ -6,6 +8,48 @@ if __name__ == "__main__":
 from implemented import resolve_implemented_cards
 from fireplace import cards
 
+
+def cleanhtml(raw_html):
+	cleanr = re.compile('<.*?>')
+	cleantext = re.sub(cleanr, '', raw_html)
+	return cleantext
+
+
+def clean_text(text):
+	text = cleanhtml(text)
+	text = text.replace('\n', ' ')
+	text = text.replace('_', ' ')
+	# text = text.replace('\'s', '')
+	# text = text.replace('@', '')
+	text = text.replace('\'', '')
+	text = text.replace('\"', '')
+	text = text.replace(']', ' ')
+	# text = text.replace('\'re', '')
+
+	text = text.translate(str.maketrans('', '', '@.,:;[]()!’-'))
+
+	return text
+
+
+def levenshtein(a, b):
+	"Calculates the Levenshtein distance between a and b."
+	n, m = len(a), len(b)
+	if n > m:
+		# Make sure n <= m, to use O(min(n,m)) space
+		a, b = b, a
+		n, m = m, n
+
+	current = range(n + 1)
+	for i in range(1, m + 1):
+		previous, current = current, [i] + [0] * n
+		for j in range(1, n + 1):
+			add, delete = previous[j] + 1, current[j - 1] + 1
+			change = previous[j - 1]
+			if a[j - 1] != b[i - 1]:
+				change = change + 1
+			current[j] = min(add, delete, change)
+
+	return current[n]
 
 class CardImplementationHelper():
 	"""
@@ -29,11 +73,16 @@ class CardImplementationHelper():
 def main():
 	helper = CardImplementationHelper()
 	print("Some unimplemented cards: ", end="")
-	print(list(filter(lambda x: x.card_set.is_standard, helper.unimplemented))[:])
+	print(list(filter(lambda x: x.card_set.is_standard, helper.unimplemented))[:10])
 	card = helper.search_card()
 
 	# script = get_script_definition(card.id)
-	# globalstrings = load_globalstrings()
+
+
+	distances = sorted(helper.implemented, key=lambda x: levenshtein(clean_text(x.description), clean_text(card.description)))[:10]
+
+	print(*["{}: {}".format(c.id, clean_text(c.description)) for c in distances], sep='\n')
+
 
 	template = "class {id}:\n" \
 			   "\t\"{name}\n\t{description}\"\n\tplay = None".format(id=card.id, name=card.name,
