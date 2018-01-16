aeae2f3c5c86ad84c88579cce8e8f31c8eea6fa5
semitable <almak72@gmail.com>
2018-01-16 16:04:39 +0200

    Implemented Rattling Rascal

--- fireplace/cards/icecrown/collectible.py
+++ fireplace/cards/icecrown/collectible.py
@@ -3,10 +3,16 @@ from ..utils import *
 
 ##
 # Minions
+class ICC_025:
+	"Rattling Rascal"
+	play = Summon(CONTROLLER, "ICC_025t")
+	deathrattle = Summon(OPPONENT, "ICC_025t")
+
 
 class ICC_911:
 	"Keening Banshee"
 	events = Play(CONTROLLER).on(Mill(CONTROLLER, 3))
+
 
 ##
 # Spells
@@ -18,5 +24,5 @@ class ICC_911:
 class ICC_082:
 	"Frozen Clone"
 	secret = Play(OPPONENT, MINION).after(
-			Reveal(SELF), Give(CONTROLLER, Copy(Play.CARD))*2
+		Reveal(SELF), Give(CONTROLLER, Copy(Play.CARD)) * 2
 	)
--- tests/test_icecrown.py
+++ tests/test_icecrown.py
@@ -1,5 +1,6 @@
 from utils import *
 
+
 def test_keening_banshee():
 	"ICC_911"
 	game = prepare_game()
@@ -18,7 +19,7 @@ def test_keening_banshee():
 
 	game.end_turn()
 
-	game.player2.give(WISP).play() # this shouldn't affect the deck
+	game.player2.give(WISP).play()  # this shouldn't affect the deck
 	assert len(game.player1.deck) == 26 - 3 - 3
 
 
@@ -42,3 +43,23 @@ def test_frozen_clone():
 	assert game.player1.hand[-1].id == WISP
 	assert game.player1.hand[-2].id == WISP
 
+
+def test_rattling_rascal():
+	"ICC_025"
+
+	game = prepare_game()
+	rascal = game.player1.give("ICC_025")
+	rascal.play()
+	assert len(game.player1.field) == 2
+
+	assert game.player1.field[0].id == "ICC_025"
+	assert game.player1.field[1].id == "ICC_025t"
+
+	game.end_turn()
+
+	game.player2.give(SOULFIRE).play(target=rascal)
+
+	assert len(game.player1.field) == 1
+
+	assert len(game.player2.field) == 1
+	assert game.player2.field[0].id == "ICC_025t"
