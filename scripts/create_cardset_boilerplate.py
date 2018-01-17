a9f6947f7d1779bb1f48d13874dc740ff521bf6e
semitable <almak72@gmail.com>
2018-01-17 23:17:26 +0200

    Will parse old-style card defs from existing files (experimental)

--- scripts/create_cardset_boilerplate.py
+++ scripts/create_cardset_boilerplate.py
@@ -3,6 +3,7 @@ import sys
 import urllib.parse
 import re
 from itertools import groupby
+import inspect
 
 from hearthstone.enums import CardSet
 from hearthstone.stringsfile import load_globalstrings
@@ -11,6 +12,7 @@ if __name__ == '__main__':
 	sys.path.append("..")
 
 from implemented import resolve_implemented_cards
+from fireplace.utils import get_script_definition
 
 FILE_HEADER = (
 """
@@ -18,30 +20,37 @@ FILE_HEADER = (
 Card Definitions for {expansion_name}
 Part of Fireplace: The Hearthstone simulator in Python
 \"\"\"
+
+from ..utils import *
 """)
 
 CARDDEF_TEMPLATE = (
 """
 
-# class {id}:
-# \t\"\"\" 
-# \t{name} - ({type})
-# \t{description}
-# \t{link}
-# \t\"\"\"
-{battlecry}\
-{deathrattle}\
-# \tpass
+class {id}:
+	\"\"\" 
+	{name} - ({type})
+	{description}
+	{link}
+	\"\"\"
+{line1}\
+{line2}\
+{line3}\
 """)
 
 BATTLECRY_LINE = (
 """\
-# \tplay = None
+	play = None
 """
 )
 DEATHRATTLE_LINE = (
 """\
-# \tdeathrattle = None
+	deathrattle = None
+"""
+)
+PASS_LINE = (
+"""\
+	pass
 """
 )
 
@@ -74,12 +83,17 @@ def prettify_description(description):
 def main(*args):
 	parser = argparse.ArgumentParser(description='Create boilerplate for a new expansion.')
 
-	parser.add_argument('--output', type=argparse.FileType('w', encoding='UTF-8'))
+	parser.add_argument('--output', type=argparse.FileType('w', encoding='UTF-8'), help="Output file "
+																						"(use - for stdout)")
+	parser.add_argument('--parse-user-implemented', action='store_true', help="Will try to parse and include any "
+																			  "old-style card definitions. (use "
+																			  "carefully) - silent errors possible")
 	args = parser.parse_args(args)
 
 	outfile = args.output
 
 	globalstrings = load_globalstrings()
+	existing_implementation_regex = "class {id}:\n\t\"([^\"]*)\"\n((?s)(?:(?!\nclass|\n# class).)*)"
 
 	standard_card_sets = list(filter(lambda x: x.is_standard, list(CardSet)))
 
@@ -96,6 +110,16 @@ def main(*args):
 
 	implemented, unimplemented = resolve_implemented_cards()
 
+	if args.parse_user_implemented:
+		user_implemented = set(filter(lambda x:
+									x.description is not None
+									and get_script_definition(x.id) is not None,
+								  implemented))
+	else:
+		user_implemented = set()
+
+	unimplemented = user_implemented | unimplemented
+
 	unimplemented = filter(lambda x: x.card_set == card_set, unimplemented)
 	unimplemented = sorted(unimplemented, key=lambda x: (-x.card_class, x.id))
 
@@ -104,15 +128,46 @@ def main(*args):
 		outfile.write(PLAYER_CLASS_COMMENT.format(globalstrings[key.name_global]['TEXT']))
 
 		for card in group:
-			outfile.write(CARDDEF_TEMPLATE.format(
-				id=card.id,
-				name=card.name,
-				type=globalstrings[card.type.name_global]['TEXT'],
-				description=prettify_description(card.description),
-				link=make_gamepedia_link(card),
-				battlecry=BATTLECRY_LINE if card.battlecry else "",
-				deathrattle=DEATHRATTLE_LINE if card.deathrattle else "",
-			))
+
+			if card in user_implemented: # this card already has an implementation. Copy it
+
+				regex = existing_implementation_regex.format(id=card.id)
+				# find the card's file
+				f = inspect.getsourcefile(get_script_definition(card.id))
+
+				with open(f, 'r') as sourcefile:
+					source = sourcefile.read()
+					match = re.search(regex, source)
+					if match is None:
+						continue
+					if match.group(2) == "":
+						print(card.id)
+						print(repr(regex))
+						print(source)
+						print("-----")
+						print(match.group(2))
+						exit()
+					outfile.write(CARDDEF_TEMPLATE.format(
+						id=card.id,
+						name=card.name,
+						type=globalstrings[card.type.name_global]['TEXT'],
+						description=prettify_description(card.description),
+						link=make_gamepedia_link(card),
+						line1=match.group(2),
+						line2="",
+						line3=""
+					))
+			else:
+				outfile.write(CARDDEF_TEMPLATE.format(
+					id=card.id,
+					name=card.name,
+					type=globalstrings[card.type.name_global]['TEXT'],
+					description=prettify_description(card.description),
+					link=make_gamepedia_link(card),
+					line1=BATTLECRY_LINE if card.battlecry else "",
+					line2=DEATHRATTLE_LINE if card.deathrattle else "",
+					line3=PASS_LINE if not card.battlecry and not card.deathrattle else "",
+				).replace("\n", "\n# ")) # comment out the block
 
 	outfile.close()
 
