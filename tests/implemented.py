761f63d3e3433ba066b676f64bf3d98b9c9b1d8c
SoManyPants <36906916+SoManyPants@users.noreply.github.com>
2018-03-01 17:25:19 -0600

    Add Poisonous keyword to implemented.py script

--- tests/implemented.py
+++ tests/implemented.py
@@ -17,7 +17,7 @@ PREFIXES = {
 }
 
 SOLVED_KEYWORDS = [
-	"Windfury", "Charge", "Divine Shield", "Taunt", "Stealth",
+	"Windfury", "Charge", "Divine Shield", "Taunt", "Stealth", "Poisonous",
 	r"Can't be targeted by spells or Hero Powers\.",
 	r"Can't attack\.",
 	"Destroy any minion damaged by this minion.",
