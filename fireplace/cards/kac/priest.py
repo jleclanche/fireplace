eac13847138e20f1325f1713cb8d57b27e7ac966
SoManyPants <36906916+SoManyPants@users.noreply.github.com>
2018-03-04 19:46:02 -0600

    Add 'Psychic Scream' and test

--- fireplace/cards/kac/priest.py
+++ fireplace/cards/kac/priest.py
@@ -1,1 +1,14 @@
 from ..utils import *
+
+##
+# Minions
+
+
+##
+# Spells
+
+class LOOT_008:
+	"Psychic Scream"
+	play = Steal(FRIENDLY_MINIONS, OPPONENT), Shuffle(OPPONENT, ALL_MINIONS)
+	# The following fails for friendly minions:
+	#play = Shuffle(OPPONENT, ALL_MINIONS)
--- tests/test_kac.py
+++ tests/test_kac.py
@@ -1,1 +1,27 @@
 from utils import *
+
+def test_psychic_scream():
+	game = prepare_game()
+	decksize = len(game.player1.deck)
+	wisp1 = game.player1.give(WISP).play()
+	wisp2 = game.player1.give(WISP).play()
+	wisp3 = game.player1.give(WISP).play()
+	wisp4 = game.player1.give(WISP).play()
+	wisp5 = game.player1.give(WISP).play()
+	wisp6 = game.player1.give(WISP).play()
+	wisp7 = game.player1.give(WISP).play()
+	game.end_turn()
+	
+	wisp8 = game.player2.give(WISP).play()
+	wisp9 = game.player2.give(WISP).play()
+	wisp10 = game.player2.give(WISP).play()
+	wisp11 = game.player2.give(WISP).play()
+	wisp12 = game.player2.give(WISP).play()
+	wisp13 = game.player2.give(WISP).play()
+	wisp14 = game.player2.give(WISP).play()
+	assert len(game.player1.field)==7
+	assert len(game.player2.field)==7
+	game.player2.give("LOOT_008").play()
+	assert len(game.player1.field)==0
+	assert len(game.player2.field)==0
+	assert len(game.player1.deck)==decksize+14
