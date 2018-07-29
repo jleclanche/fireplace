979516e05ca74fe0e14a563808e1a5679ac5d35a
Ye Yun <gmfengye@gmail.com>
2018-07-29 10:58:27 +0800

    fix flake8
    keep sync with shinoi2's branch

--- fireplace/cards/gangs/mage.py
+++ fireplace/cards/gangs/mage.py
@@ -6,7 +6,7 @@ from ..utils import *
 
 class OG_087:
     """Servant of Yogg-Saron"""
-    play = CastSpell(RandomSpell(cost=[0,1,2,3,4,5]))
+    play = CastSpell(RandomSpell(cost=[0, 1, 2, 3, 4, 5]))
 
 
 ##
