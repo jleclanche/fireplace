7bbbf0de2e2506a3d99678927e68c748e0add751
shinoi2 <shinoi@mail.hfut.edu.cn>
2018-07-29 16:35:19 +0800

    fix flake8

--- fireplace/cards/wog/paladin.py
+++ fireplace/cards/wog/paladin.py
@@ -67,7 +67,7 @@ OG_222e = buff(+1, +1)
 
 
 class OG_198:
-	"Forbidden Healing"
+	"""Forbidden Healing"""
 	play = (
 		Heal(TARGET, CURRENT_MANA(CONTROLLER) * 2),
 		SpendMana(CONTROLLER, CURRENT_MANA(CONTROLLER))
