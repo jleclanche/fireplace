ad1489db6ec17c5cf1d36ad6c6cfc76394d8d3fc
shinoi2 <shinoi@mail.hfut.edu.cn>
2018-07-29 22:32:06 +0800

    Renounce Darkness(OG_118) impl

--- fireplace/cards/wog/warlock.py
+++ fireplace/cards/wog/warlock.py
@@ -41,6 +41,36 @@ class OG_116:
 	play = Hit(RANDOM_CHARACTER, 1) * 9
 
 
+class OG_118:
+	"""Renounce Darkness"""
+	def play(self):
+		import random
+		classes = [
+			(CardClass.DRUID, "CS2_017"),
+			(CardClass.HUNTER, "DS1h_292"),
+			(CardClass.MAGE, "CS2_034"),
+			(CardClass.PALADIN, "CS2_101"),
+			(CardClass.PRIEST, "CS1h_001"),
+			(CardClass.ROGUE, "CS2_083b"),
+			(CardClass.SHAMAN, "CS2_049"),
+			(CardClass.WARRIOR, "CS2_102")
+		]
+		hero_class, hero_power = random.choice(classes)
+		yield Summon(CONTROLLER, hero_power)
+		yield Morph(
+			FRIENDLY + (IN_DECK | IN_HAND) + WARLOCK,
+			RandomCollectible(card_class = hero_class)
+		).then(
+			Buff(Morph.CARD, "OG_118e")
+		)
+
+
+class OG_118e:
+	events = REMOVED_IN_PLAY
+	tags = {GameTag.COST: -1}
+
+
+
 class OG_239:
 	"""DOOM!"""
 	def play(self):
