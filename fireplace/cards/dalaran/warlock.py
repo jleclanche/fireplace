b963a0be2907d6868dcc679fc9e0b950a3e67a67
shinoi2 <shinoi@mail.hfut.edu.cn>
2024-03-13 16:34:48 +0800

    warlock

--- fireplace/cards/dalaran/warlock.py
+++ fireplace/cards/dalaran/warlock.py
@@ -7,40 +7,56 @@ from ..utils import *
 class DAL_185:
 	"""Aranasi Broodmother"""
 	# [x]<b>Taunt</b> When you draw this, restore #4 Health to your hero.
-	pass
+	draw = Heal(FRIENDLY_HERO, 4)
 
 
 class DAL_422:
 	"""Arch-Villain Rafaam"""
 	# <b><b>Taunt</b> Battlecry:</b> Replace your hand and deck with <b>Legendary</b>
 	# minions.
-	pass
+	play = Morph(FRIENDLY_HAND + FRIENDLY_DECK, RandomLegendaryMinion())
 
 
 class DAL_561:
 	"""Jumbo Imp"""
 	# Costs (1) less whenever a friendly Demon dies while this is in your hand.
-	pass
+	class Hand:
+		events = Death(FRIENDLY + DEMON).on(Buff(SELF, "DAL_561e"))
+
+
+class GVG_063e:
+	tags = {GameTag.COST: -1}
+	events = REMOVED_IN_PLAY
 
 
 class DAL_563:
 	"""Eager Underling"""
 	# <b>Deathrattle:</b> Give two random friendly minions +2/+2.
-	pass
+	deathrattle = Buff(RANDOM_OTHER_FRIENDLY_MINION * 2, "DAL_563e")
+
+
+DAL_563e = buff(+2, +2)
 
 
 class DAL_606:
 	"""EVIL Genius"""
 	# <b>Battlecry:</b> Destroy a friendly minion to add 2 random
 	# <b>Lackeys</b>_to_your_hand.
-	pass
+	play = Destroy(TARGET), Give(CONTROLLER, RandomLackey() * 2)
 
 
 class DAL_607:
 	"""Fel Lord Betrug"""
 	# [x]Whenever you draw a minion, summon a copy with <b>Rush</b> that dies at end of
 	# turn.
-	pass
+	events = Draw(CONTROLLER).on(
+		Summon(CONTROLLER, Copy(Draw.CARD)).then(Buff(Summon.CARD, "DAL_607e"))
+	)
+
+
+class DAL_607e:
+	tags = {GameTag.RUSH: True}
+	events = OWN_TURN_END.on(Destroy(OWNER))
 
 
 ##
@@ -49,22 +65,28 @@ class DAL_607:
 class DAL_007:
 	"""Rafaam's Scheme"""
 	# Summon @ 1/1 |4(Imp, Imps). <i>(Upgrades each turn!)</i>
-	pass
+	play = Summon(CONTROLLER, "DAL_751t") * Attr(SELF, GameTag.QUEST_PROGRESS) + Number(1)
 
 
 class DAL_173:
 	"""Darkest Hour"""
 	# Destroy all friendly minions. For each one, summon a random minion from your deck.
-	pass
+	play = Destroy(FRIENDLY_MINIONS).then(Summon(CONTROLLER, RANDOM(FRIENDLY_DECK + MINION)))
 
 
 class DAL_602:
 	"""Plot Twist"""
 	# Shuffle your hand into your deck. Draw that many cards.
-	pass
+	def play(self):
+		count = len(self.controller.hand)
+		yield Shuffle(FRIENDLY_HAND)
+		yield Draw(CONTROLLER) * count
 
 
 class DAL_605:
 	"""Impferno"""
 	# Give your Demons +1 Attack. Deal $1 damage to all enemy minions.
-	pass
+	play = Buff(FRIENDLY_MINIONS + DEMON, "DAL_605e"), Hit(ENEMY_MINIONS, 1)
+
+
+DAL_605e = buff(+1, +1)
