571bfcc26c5f3daddb8ada4e131a42146e2da40e
shinoi2 <shinoi@mail.hfut.edu.cn>
2024-03-21 11:04:07 +0800

    warlock and warrior

--- fireplace/cards/uldum/warlock.py
+++ fireplace/cards/uldum/warlock.py
@@ -7,7 +7,7 @@ from ..utils import *
 class ULD_161:
 	"""Neferset Thrasher"""
 	# Whenever this attacks, deal 3 damage to your_hero.
-	pass
+	events = Attack(SELF).on(Hit(FRIENDLY_HERO, 3))
 
 
 class ULD_162:
@@ -19,14 +19,23 @@ class ULD_162:
 		PlayReq.REQ_MINION_TARGET: 0,
 		PlayReq.REQ_FRIENDLY_LACKEY: 0,
 	}
-	pass
+	play = Destroy(TARGET), Summon(CONTROLLER, "ULD_162t")
 
 
 class ULD_163:
 	"""Expired Merchant"""
 	# [x]<b>Battlecry:</b> Discard your highest Cost card. <b>Deathrattle:</b> Add 2 copies
 	# of it to your hand.
-	pass
+	play = Discard(RANDOM(HIGHEST_COST(FRIENDLY_HAND))).then(
+		StoringBuff(SELF, "ULD_163e", Discard.CARD)
+	)
+
+
+class ULD_163e:
+	tags = {GameTag.DEATHRATTLE: True}
+
+	def deathrattle(self):
+		yield Give(CONTROLLER, self.store_card.id) * 2
 
 
 class ULD_165:
@@ -36,19 +45,33 @@ class ULD_165:
 		PlayReq.REQ_TARGET_TO_PLAY: 0,
 		PlayReq.REQ_MINION_TARGET: 0,
 	}
-	pass
+	play = Hit(FRIENDLY_HERO, CURRENT_HEALTH(TARGET)), Destroy(TARGET)
 
 
 class ULD_167:
 	"""Diseased Vulture"""
 	# After your hero takes damage on your turn, summon a random 3-Cost minion.
-	pass
+	events = Hit(FRIENDLY_HERO).on(
+		CURRENT_PLAYER(CONTROLLER) & Summon(CONTROLLER, RandomMinion(cost=3))
+	)
 
 
 class ULD_168:
 	"""Dark Pharaoh Tekahn"""
 	# <b>Battlecry:</b> For the rest of the game, your <b>Lackeys</b> are 4/4.
-	pass
+	play = Buff(CONTROLLER, "ULD_168e")
+
+
+class ULD_168e:
+	update = Refresh(
+		(IN_DECK | IN_HAND | IN_PLAY) + FRIENDLY + LACKEY,
+		buff="ULD_168e3"
+	)
+
+
+class ULD_168e3:
+	atk = SET(4)
+	max_health = SET(4)
 
 
 ##
@@ -57,13 +80,26 @@ class ULD_168:
 class ULD_140:
 	"""Supreme Archaeology"""
 	# <b>Quest:</b> Draw 20 cards. <b>Reward:</b> Tome of Origination.
-	pass
+	progress_total = 20
+	quest = Draw(CONTROLLER).after(AddProgress(SELF, Draw.CARD))
+	reward = Summon(CONTROLLER, "ULD_140p")
+
+
+class ULD_140p:
+	"""Tome of Origination"""
+	# <b>Hero Power</b> Draw a card. It costs (0).
+	activate = Draw(CONTROLLER).then(Buff(Draw.CARD, "ULD_140e"))
+
+
+class ULD_140e:
+	cost = SET(0)
+	events = REMOVED_IN_PLAY
 
 
 class ULD_160:
 	"""Sinister Deal"""
 	# <b>Discover</b> a <b>Lackey</b>.
-	pass
+	play = DISCOVER(RandomLackey())
 
 
 class ULD_324:
@@ -73,10 +109,13 @@ class ULD_324:
 		PlayReq.REQ_TARGET_TO_PLAY: 0,
 		PlayReq.REQ_MINION_TARGET: 0,
 	}
-	pass
+	play = Destroy(TARGET), Shuffle(CONTROLLER, "ULD_324t") * 3
 
 
 class ULD_717:
 	"""Plague of Flames"""
 	# [x]Destroy all your minions. For each one, destroy a random enemy minion.
-	pass
+	def play(self):
+		count = Count(FRIENDLY_MINIONS).evaluate(self)
+		yield Destroy(FRIENDLY_MINIONS)
+		yield Destroy(RANDOM_ENEMY_MINION * count)
--- fireplace/cards/uldum/warrior.py
+++ fireplace/cards/uldum/warrior.py
@@ -7,26 +7,29 @@ from ..utils import *
 class ULD_195:
 	"""Frightened Flunky"""
 	# <b>Taunt</b> <b>Battlecry:</b> <b>Discover</b> a <b>Taunt</b>_minion.
-	pass
+	play = DISCOVER(RandomMinion(taunt=True))
 
 
 class ULD_253:
 	"""Tomb Warden"""
 	# <b>Taunt</b> <b>Battlecry:</b> Summon a copy of this minion.
-	pass
+	play = Summon(CONTROLLER, ExactCopy(SELF))
 
 
 class ULD_258:
 	"""Armagedillo"""
 	# [x]<b>Taunt</b> At the end of your turn, give all <b>Taunt</b> minions in your hand
 	# +2/+2.
-	pass
+	events = OWN_TURN_END.on(Buff(FRIENDLY_HAND + MINION + TAUNT, "ULD_258e"))
+
+
+ULD_258e = buff(+2, +2)
 
 
 class ULD_709:
 	"""Armored Goon"""
 	# Whenever your hero attacks, gain 5 Armor.
-	pass
+	events = Attack(FRIENDLY_HERO).on(GainArmor(FRIENDLY_HERO, 5))
 
 
 class ULD_720:
@@ -38,7 +41,7 @@ class ULD_720:
 		PlayReq.REQ_MINION_TARGET: 0,
 		PlayReq.REQ_DAMAGED_TARGET: 0,
 	}
-	pass
+	play = Summon(CONTROLLER, ExactCopy(TARGET))
 
 
 ##
@@ -47,19 +50,34 @@ class ULD_720:
 class ULD_256:
 	"""Into the Fray"""
 	# Give all <b>Taunt</b> minions in your hand +2/+2.
-	pass
+	play = Buff(FRIENDLY_HAND + MINION + TAUNT, "ULD_256e")
+
+
+ULD_256e = buff(+2, +2)
 
 
 class ULD_707:
 	"""Plague of Wrath"""
 	# Destroy all damaged minions.
-	pass
+	play = Destroy(ALL_MINIONS + DAMAGED)
 
 
 class ULD_711:
 	"""Hack the System"""
 	# [x]<b>Quest:</b> Attack 5 times with your hero. <b>Reward:</b> Anraphet's Core.
-	pass
+	progress_total = 5
+	quest = Attack(FRIENDLY_HERO).after(AddProgress(SELF, Attack.ATTACKER))
+	reward = Summon(CONTROLLER, "ULD_711p3")
+
+
+class ULD_711p3:
+	"""Anraphet's Core"""
+	# [x]<b>Hero Power</b> Summon a 4/3 Golem. After your hero attacks, refresh this.
+	requirements = {
+		PlayReq.REQ_NUM_MINION_SLOTS: 1,
+	}
+	activate = Summon(CONTROLLER, "ULD_711t")
+	events = Attack(FRIENDLY_HERO).after(RefreshHeroPower(SELF))
 
 
 ##
@@ -68,4 +86,4 @@ class ULD_711:
 class ULD_708:
 	"""Livewire Lance"""
 	# After your Hero attacks, add a <b>Lackey</b> to your_hand.
-	pass
+	events = Attack(FRIENDLY_HERO).after(Give(CONTROLLER, RandomLackey()))
