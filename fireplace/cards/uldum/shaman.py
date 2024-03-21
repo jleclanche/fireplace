329a06df9896f2e23be725f8248855b6718faa31
shinoi2 <shinoi@mail.hfut.edu.cn>
2024-03-21 10:41:04 +0800

    shaman

--- fireplace/cards/uldum/shaman.py
+++ fireplace/cards/uldum/shaman.py
@@ -7,13 +7,13 @@ from ..utils import *
 class ULD_158:
 	"""Sandstorm Elemental"""
 	# <b>Battlecry:</b> Deal 1 damage to all enemy minions. <b>Overload:</b> (1)
-	pass
+	play = Heal(ENEMY_MINIONS, 1)
 
 
 class ULD_169:
 	"""Mogu Fleshshaper"""
 	# [x]<b>Rush</b>. Costs (1) less for each minion on the battlefield.
-	pass
+	cost_mod = -Count(ALL_MINIONS)
 
 
 class ULD_170:
@@ -22,19 +22,19 @@ class ULD_170:
 	requirements = {
 		PlayReq.REQ_TARGET_IF_AVAILABLE_AND_FRIENDLY_LACKEY: 0,
 	}
-	pass
+	play = Hit(TARGET, 3)
 
 
 class ULD_173:
 	"""Vessina"""
 	# While you're <b>Overloaded</b>, your other minions have +2 Attack.
-	pass
+	update = OVERLOADED(CONTROLLER) & Refresh(FRIENDLY_MINIONS - SELF, {GameTag.ATK: 2})
 
 
 class ULD_276:
 	"""EVIL Totem"""
 	# At the end of your turn, add a <b>Lackey</b> to your hand.
-	pass
+	events = OWN_TURN_END.on(Give(CONTROLLER, RandomLackey()))
 
 
 ##
@@ -43,25 +43,40 @@ class ULD_276:
 class ULD_171:
 	"""Totemic Surge"""
 	# Give your Totems +2_Attack.
-	pass
+	play = Buff(FRIENDLY_MINIONS + TOTEM, "ULD_171e")
+
+
+ULD_171e = buff(atk=2)
 
 
 class ULD_172:
 	"""Plague of Murlocs"""
 	# Transform all minions into random Murlocs.
-	pass
+	play = Morph(ALL_MINIONS, RandomMurloc())
 
 
 class ULD_181:
 	"""Earthquake"""
 	# Deal $5 damage to all minions, then deal $2 damage to all minions.
-	pass
+	play = Hit(ALL_MINIONS, 5), Hit(ALL_MINIONS, 2)
 
 
 class ULD_291:
 	"""Corrupt the Waters"""
 	# [x]<b>Quest:</b> Play 6 <b>Battlecry</b> cards. <b>Reward:</b> Heart of Vir'naal.
-	pass
+	progress_total = 6
+	quest = Play(CONTROLLER, BATTLECRY).after(AddProgress(SELF, Play.CARD))
+	reward = Summon(CONTROLLER, "ULD_291p")
+
+
+class ULD_291p:
+	"""Heart of Vir'naal"""
+	# <b>Hero Power</b> Your <b>Battlecries</b> trigger twice this turn.
+	activate = Buff(CONTROLLER, "ULD_291pe")
+
+
+class ULD_291pe:
+	tags = {enums.EXTRA_BATTLECRIES: True}
 
 
 ##
@@ -70,4 +85,4 @@ class ULD_291:
 class ULD_413:
 	"""Splitting Axe"""
 	# <b>Battlecry:</b> Summon copies of your Totems.
-	pass
+	play = Summon(CONTROLLER, ExactCopy(FRIENDLY_MINIONS + TOTEM))
