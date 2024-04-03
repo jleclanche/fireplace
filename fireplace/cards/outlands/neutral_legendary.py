05be458be0f54885788cdb91ddd2cd242bbd7b6e
shinoi2 <shinoi@mail.hfut.edu.cn>
2024-04-03 16:45:53 +0800

    Kael'thas Sunstrider

--- fireplace/cards/outlands/neutral_legendary.py
+++ fireplace/cards/outlands/neutral_legendary.py
@@ -7,4 +7,10 @@ from ..utils import *
 class BT_255:
 	"""Kael'thas Sunstrider"""
 	# Every third spell you cast each turn costs (0).
-	pass
+	update = (Count(CARDS_PLAYED_THIS_TURN + SPELL) % Number(3) == 2) & (
+		Refresh(FRIENDLY_HAND + SPELL, buff="BT_255e")
+	)
+
+
+class BT_255e:
+	cost = SET(0)
